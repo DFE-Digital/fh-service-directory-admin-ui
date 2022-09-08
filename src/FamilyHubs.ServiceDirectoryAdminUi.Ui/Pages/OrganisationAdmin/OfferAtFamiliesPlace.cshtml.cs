c32aa81a09ecfc222b1286488f508c7fea514dd1
palitsyndfe <anton.palitsyn@digital.education.gov.uk>
2022-09-08 09:23:55 +0100

    Added correct back button

--- src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/OfferAtFamiliesPlace.cshtml.cs
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/OfferAtFamiliesPlace.cshtml.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FamilyHubs.ServiceDirectory.Shared.Enums;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
-namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.Pages.OrganisationAdmin
+namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.Pages.OrganisationAdmin;
+
+
+public class OfferAtFamiliesPlaceModel : PageModel
 {
-    public class OfferAtFamiliesPlaceModel : PageModel
+    [BindProperty]
+    public string? StrOrganisationViewModel { get; set; }
+
+    public void OnGet(string strOrganisationViewModel)
     {
-        public void OnGet()
-        {
-        }
+        StrOrganisationViewModel = strOrganisationViewModel;
     }
 }
+
