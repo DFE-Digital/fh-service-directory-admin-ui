10f0b182e83cbf40ec3187ee8b97d2370c46976b
palitsyndfe <anton.palitsyn@digital.education.gov.uk>
2022-09-08 08:25:55 +0100

    Added a placeholder for "OfferAtFamiliesPlace"

---
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/OfferAtFamiliesPlace.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.Pages.OrganisationAdmin
+{
+    public class OfferAtFamiliesPlaceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
