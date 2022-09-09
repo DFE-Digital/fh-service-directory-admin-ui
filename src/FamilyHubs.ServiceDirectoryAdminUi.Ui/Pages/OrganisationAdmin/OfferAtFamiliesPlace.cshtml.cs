57bc8bb40379e0c011700b299456d1f38f3ae749
palitsyndfe <anton.palitsyn@digital.education.gov.uk>
2022-09-09 11:45:30 +0100

    Layout created and validation mocked.

--- src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/OfferAtFamiliesPlace.cshtml.cs
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/OfferAtFamiliesPlace.cshtml.cs
@@ -1,20 +1,51 @@
-﻿using FamilyHubs.ServiceDirectory.Shared.Enums;
-using FamilyHubs.ServiceDirectoryAdminUi.Ui.Models;
+﻿using FamilyHubs.ServiceDirectoryAdminUi.Ui.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 
 namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.Pages.OrganisationAdmin;
 
-
 public class OfferAtFamiliesPlaceModel : PageModel
 {
     [BindProperty]
+    public string Familychoice { get; set; } = default!;
+
+    [BindProperty]
+    public OrganisationViewModel OrganisationViewModel { get; set; } = new OrganisationViewModel();
+
+    [BindProperty]
     public string? StrOrganisationViewModel { get; set; }
+
+    [BindProperty]
+    public bool ValidationValid { get; set; } = true;
 
     public void OnGet(string strOrganisationViewModel)
     {
         StrOrganisationViewModel = strOrganisationViewModel;
+        if (!string.IsNullOrEmpty(strOrganisationViewModel))
+        {
+            OrganisationViewModel = JsonConvert.DeserializeObject<OrganisationViewModel>(StrOrganisationViewModel) ?? new OrganisationViewModel();
+            if (!string.IsNullOrEmpty(OrganisationViewModel.Familychoice))
+            {
+                Familychoice = OrganisationViewModel.Familychoice;
+            }
+        }
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!string.IsNullOrEmpty(StrOrganisationViewModel))
+        {
+            OrganisationViewModel = JsonConvert.DeserializeObject<OrganisationViewModel>(StrOrganisationViewModel) ?? new OrganisationViewModel();
+        }
+
+        OrganisationViewModel.Familychoice = Familychoice;
+
+        StrOrganisationViewModel = JsonConvert.SerializeObject(OrganisationViewModel);
+
+        return RedirectToPage("/OrganisationAdmin/WhoFor", new
+        {
+            StrOrganisationViewModel
+        });
     }
 }
-
