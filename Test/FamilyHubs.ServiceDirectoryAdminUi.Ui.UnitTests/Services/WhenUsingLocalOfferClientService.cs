f7992f17d3612a8a19958659aef343f47fe4dc35
MartinBelton-gov <111495215+MartinBelton-gov@users.noreply.github.com>
2022-09-14 08:44:46 +0100

    Added OpenReferralOrganisationId to OpenReferralServiceDto (#10)

--- Test/FamilyHubs.ServiceDirectoryAdminUi.Ui.UnitTests/Services/WhenUsingLocalOfferClientService.cs
+++ Test/FamilyHubs.ServiceDirectoryAdminUi.Ui.UnitTests/Services/WhenUsingLocalOfferClientService.cs
@@ -18,7 +18,7 @@ public class WhenUsingLocalOfferClientService : BaseClientService
         //Arrange
         List<OpenReferralServiceDto> list = new()
         {
-            GetTestCountyCouncilServicesDto()
+            GetTestCountyCouncilServicesDto("56e62852-1b0b-40e5-ac97-54a67ea957dc")
         };
 
         PaginatedList<OpenReferralServiceDto> paginatedList = new();
@@ -40,7 +40,7 @@ public class WhenUsingLocalOfferClientService : BaseClientService
     public async Task ThenGetLocalOfferById()
     {
         //Arrange
-        var service = GetTestCountyCouncilServicesDto();
+        var service = GetTestCountyCouncilServicesDto("56e62852-1b0b-40e5-ac97-54a67ea957dc");
         var json = JsonConvert.SerializeObject(service);
         var mockClient = GetMockClient(json);
         LocalOfferClientService localOfferClientService = new(mockClient);
@@ -60,7 +60,7 @@ public class WhenUsingLocalOfferClientService : BaseClientService
         //Arrange
         List<OpenReferralServiceDto> list = new()
         {
-            GetTestCountyCouncilServicesDto()
+            GetTestCountyCouncilServicesDto("56e62852-1b0b-40e5-ac97-54a67ea957dc")
         };
 
         var json = JsonConvert.SerializeObject(list);
