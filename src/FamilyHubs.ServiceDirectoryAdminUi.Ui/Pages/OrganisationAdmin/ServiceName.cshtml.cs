8bb452274747eb1b611b3a23bf1f0bed3716f7e0
AliShah2 <kishwer01@gmail.com>
2022-09-24 13:42:28 +0100

    Added session storage to ServiceName page

--- src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/ServiceName.cshtml.cs
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/ServiceName.cshtml.cs
@@ -1,4 +1,5 @@
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralOrganisations;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Extensions;
 using FamilyHubs.ServiceDirectoryAdminUi.Ui.Models;
 using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services;
 using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services.Api;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using static FamilyHubs.ServiceDirectoryAdminUi.Ui.Infrastructure.Configuration.SessionConfiguration;
 
 namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.Pages.OrganisationAdmin;
 
@@ -16,11 +18,12 @@ public class ServiceNameModel : PageModel
     [Required(ErrorMessage = "You must enter a service name")]
     public string ServiceName { get; set; } = default!;
 
-    [BindProperty]
-    public string? StrOrganisationViewModel { get; set; }
+    //[BindProperty]
+    //public string? StrOrganisationViewModel { get; set; }
 
     [BindProperty]
     public bool ValidationValid { get; set; } = true;
+    //public string SessionKeyService { get; private set; }
 
     private readonly IOpenReferralOrganisationAdminClientService _openReferralOrganisationAdminClientService;
 
@@ -31,18 +34,9 @@ public class ServiceNameModel : PageModel
 
     public async Task OnGet(string organisationid, string serviceid, string strOrganisationViewModel)
     {
-        if (!string.IsNullOrEmpty(strOrganisationViewModel))
-        {
-            StrOrganisationViewModel = strOrganisationViewModel;
-
-            var organisationViewModel = JsonConvert.DeserializeObject<OrganisationViewModel>(StrOrganisationViewModel) ?? new OrganisationViewModel();
-            if (organisationViewModel != null)
-            {
-                if (!string.IsNullOrEmpty(organisationViewModel.ServiceName))
-                    ServiceName = organisationViewModel.ServiceName;
-            }
-        }
-        else
+        /*** Using Session storage ***/
+        //if service not stored in session, get from api, else get from session storage
+        if (HttpContext.Session.Get<OrganisationViewModel>(SessionKeyService) == default)
         {
             OpenReferralOrganisationWithServicesDto openReferralOrganisation = await _openReferralOrganisationAdminClientService.GetOpenReferralOrganisationById(organisationid);
             var vm = ApiModelToViewModelHelper.CreateViewModel(openReferralOrganisation, serviceid);
@@ -50,10 +44,38 @@ public class ServiceNameModel : PageModel
             {
                 if (!string.IsNullOrEmpty(vm.ServiceName))
                     ServiceName = vm.ServiceName;
-                StrOrganisationViewModel = JsonConvert.SerializeObject(vm);
+                HttpContext.Session.Set<OrganisationViewModel>(SessionKeyService, vm);
             }
-
         }
+        else
+        {
+            var vm = HttpContext.Session.Get<OrganisationViewModel>(SessionKeyService);
+            ServiceName = vm?.ServiceName ?? "";
+        }
+
+        //if (!string.IsNullOrEmpty(strOrganisationViewModel))
+        //{
+        //    StrOrganisationViewModel = strOrganisationViewModel;
+
+        //    var organisationViewModel = JsonConvert.DeserializeObject<OrganisationViewModel>(StrOrganisationViewModel) ?? new OrganisationViewModel();
+        //    if (organisationViewModel != null)
+        //    {
+        //        if (!string.IsNullOrEmpty(organisationViewModel.ServiceName))
+        //            ServiceName = organisationViewModel.ServiceName;
+        //    }
+        //}
+        //else
+        //{
+        //    OpenReferralOrganisationWithServicesDto openReferralOrganisation = await _openReferralOrganisationAdminClientService.GetOpenReferralOrganisationById(organisationid);
+        //    var vm = ApiModelToViewModelHelper.CreateViewModel(openReferralOrganisation, serviceid);
+        //    if (vm != null)
+        //    {
+        //        if (!string.IsNullOrEmpty(vm.ServiceName))
+        //            ServiceName = vm.ServiceName;
+        //        StrOrganisationViewModel = JsonConvert.SerializeObject(vm);
+        //    }
+
+        //}
 
     }
 
@@ -65,18 +87,29 @@ public class ServiceNameModel : PageModel
             return Page();
         }
 
-        if (StrOrganisationViewModel != null)
+        var vm = HttpContext.Session.Get<OrganisationViewModel>(SessionKeyService);
+
+        if (vm == null)
         {
-            var organisationViewModel = JsonConvert.DeserializeObject<OrganisationViewModel>(StrOrganisationViewModel) ?? new OrganisationViewModel();
-
-            organisationViewModel.ServiceName = ServiceName;
-
-            StrOrganisationViewModel = JsonConvert.SerializeObject(organisationViewModel);
+            vm = new OrganisationViewModel();
         }
+        vm.ServiceName = ServiceName;
+        HttpContext.Session.Set(SessionKeyService, vm);
 
-        return RedirectToPage("/OrganisationAdmin/TypeOfService", new
-        {
-            strOrganisationViewModel = StrOrganisationViewModel
-        });
+        //if (StrOrganisationViewModel != null)
+        //{
+        //    var organisationViewModel = JsonConvert.DeserializeObject<OrganisationViewModel>(StrOrganisationViewModel) ?? new OrganisationViewModel();
+
+        //    organisationViewModel.ServiceName = ServiceName;
+
+        //    StrOrganisationViewModel = JsonConvert.SerializeObject(organisationViewModel);
+        //}
+
+        //return RedirectToPage("/OrganisationAdmin/TypeOfService", new
+        //{
+        //    strOrganisationViewModel = StrOrganisationViewModel
+        //});
+
+        return Page();
     }
 }
