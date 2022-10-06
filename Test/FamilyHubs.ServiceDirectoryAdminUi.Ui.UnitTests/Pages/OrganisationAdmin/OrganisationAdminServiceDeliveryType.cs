2e0179b74cf0a1ed142e2979f71b89ba99fd05ea
AliShah2 <kishwer01@gmail.com>
2022-10-06 19:42:58 +0100

    Added unit tests for ServiceDeliveryType and TypeOfService

---
+++ Test/FamilyHubs.ServiceDirectoryAdminUi.Ui.UnitTests/Pages/OrganisationAdmin/OrganisationAdminServiceDeliveryType.cs
@@ -0,0 +1,47 @@
+﻿using FamilyHubs.ServiceDirectoryAdminUi.Ui.Pages.OrganisationAdmin;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services.Api;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.UnitTests.Pages.OrganisationAdmin
+{
+    public class OrganisationAdminServiceDeliveryType
+    {
+        private ServiceDeliveryTypeModel serviceDeliveryTypeModel;
+
+        public OrganisationAdminServiceDeliveryType()
+        {
+            var mockISessionService = new Mock<ISessionService>();
+            serviceDeliveryTypeModel = new ServiceDeliveryTypeModel(mockISessionService.Object);
+            serviceDeliveryTypeModel.ServiceDeliverySelection = new List<string>();
+        }
+
+        [Fact]
+        public void ValidationShouldFailWhenNoOptionSelected()
+        {
+            // Act
+            var result = serviceDeliveryTypeModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            serviceDeliveryTypeModel.ModelState.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ValidationShouldNotFailWhenAnOptionSelected()
+        {
+            //Arrange
+            serviceDeliveryTypeModel.ServiceDeliverySelection.Add("Online");
+
+            // Act
+            var result = serviceDeliveryTypeModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            serviceDeliveryTypeModel.ModelState.IsValid.Should().BeTrue();
+        }
+    }
+}
---
+++ Test/FamilyHubs.ServiceDirectoryAdminUi.Ui.UnitTests/Pages/OrganisationAdmin/OrganisationAdminTypeOfService.cs
@@ -0,0 +1,51 @@
+﻿using FamilyHubs.ServiceDirectoryAdminUi.Ui.Pages.OrganisationAdmin;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services.Api;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.UnitTests.Pages.OrganisationAdmin
+{
+    public class OrganisationAdminTypeOfService
+    {
+        private TypeOfServiceModel typeOfServiceModel;
+
+        public OrganisationAdminTypeOfService()
+        {
+            var mockOpenReferralOrganisationAdminCLientService = new Mock<IOpenReferralOrganisationAdminClientService>();
+            var mockISessionService = new Mock<ISessionService>();
+            typeOfServiceModel = new TypeOfServiceModel(mockOpenReferralOrganisationAdminCLientService.Object, mockISessionService.Object);
+        }
+
+        [Fact]
+        public async Task ValidationShouldFailWhenNoOptionSelected()
+        {
+            //Arrange
+            typeOfServiceModel.TaxonomySelection = new List<string>();
+
+            // Act
+            var result = (await typeOfServiceModel.OnPost()) as RedirectToPageResult;
+
+            // Assert
+            typeOfServiceModel.ModelState.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task ValidationShouldNotFailWhenAnOptionSelected()
+        {
+            //Arrange
+            typeOfServiceModel.TaxonomySelection = new List<string>();
+            typeOfServiceModel.TaxonomySelection.Add("Children");
+
+            // Act
+            var result = (await typeOfServiceModel.OnPost()) as RedirectToPageResult;
+
+            // Assert
+            typeOfServiceModel.ModelState.IsValid.Should().BeTrue();
+        }
+    }
+}
