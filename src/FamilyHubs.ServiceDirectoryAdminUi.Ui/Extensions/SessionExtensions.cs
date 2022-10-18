8bfdb9f1133f452afe3f9bc3fc2f1358ffdad68e
AliShah2 <kishwer01@gmail.com>
2022-10-18 17:05:41 +0100

    Clear session on Welcome page OnGet

--- src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Extensions/SessionExtensions.cs
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Extensions/SessionExtensions.cs
@@ -14,4 +14,9 @@ public static class SessionExtensions
         var value = session.GetString(key);
         return value == null ? default : JsonConvert.DeserializeObject<T>(value);
     }
+
+    public static void ReSet<T>(this ISession session, string key)
+    {
+        session.SetString(key, String.Empty);
+    }
 }
