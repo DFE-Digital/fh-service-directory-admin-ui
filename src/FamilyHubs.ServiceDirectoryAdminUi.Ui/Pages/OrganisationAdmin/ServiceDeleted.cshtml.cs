2c140c4233c7f697ee50bfa09366e1c2ff61dca9
AliShah2 <kishwer01@gmail.com>
2022-12-09 11:32:41 +0000

    Fixed 500 error when deleting a service

--- src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/ServiceDeleted.cshtml.cs
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Pages/OrganisationAdmin/ServiceDeleted.cshtml.cs
@@ -1,16 +1,28 @@
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StackExchange.Redis;
 
 namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.Pages.OrganisationAdmin;
 
 public class ServiceDeletedModel : PageModel
 {
+    private readonly IRedisCacheService _redis;
+
+    public ServiceDeletedModel(IRedisCacheService redisCacheService)
+    {
+        _redis = redisCacheService;
+    }
     public void OnGet()
     {
     }
 
     public IActionResult OnPost()
     {
-        return RedirectToPage("/OrganisationAdmin/Welcome");
+        var organisation = _redis.RetrieveOrganisationWithService();
+        return RedirectToPage("/OrganisationAdmin/Welcome", new
+        {
+            organisationId = organisation?.Id,
+        });
     }
 }
