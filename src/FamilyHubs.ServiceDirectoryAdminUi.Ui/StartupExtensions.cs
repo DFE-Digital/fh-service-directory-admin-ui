d1c2479acda3da58c01a813c9e1d3bd1ed6f0df5
Hardik Desai <hardik.desai@education.gov.uk>
2022-12-14 17:08:04 +0000

    Standardized Program.cs and working Application insight logging

---
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/StartupExtensions.cs
@@ -0,0 +1,87 @@
+﻿using FamilyHubs.ServiceDirectory.Shared.Helpers;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Extensions;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services;
+using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services.Dataupload;
+using Microsoft.ApplicationInsights.Extensibility;
+using Serilog;
+using Serilog.Events;
+
+namespace FamilyHubs.ServiceDirectoryAdminUi.Ui;
+
+public static class StartupExtensions
+{
+    public static void ConfigureHost(this WebApplicationBuilder builder)
+    {
+        // ApplicationInsights
+        builder.Host.UseSerilog((_, services, loggerConfiguration) =>
+        {
+            var logLevelString = builder.Configuration["LogLevel"];
+
+            var parsed = Enum.TryParse<LogEventLevel>(logLevelString, out var logLevel);
+
+            loggerConfiguration.WriteTo.ApplicationInsights(
+                services.GetRequiredService<TelemetryConfiguration>(),
+                TelemetryConverter.Traces,
+                parsed ? logLevel : LogEventLevel.Warning);
+
+            loggerConfiguration.WriteTo.Console(
+                parsed ? logLevel : LogEventLevel.Warning);
+        });
+    }
+
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddApplicationInsightsTelemetry();
+
+        // Add services to the container.
+        services
+            .AddClientServices()
+            .AddWebUIServices(configuration);
+
+        services.AddTransient<IViewModelToApiModelHelper, ViewModelToApiModelHelper>();
+
+        services.AddTransient<IRedisCache, RedisCache>();
+        services.AddTransient<IRedisCacheService, RedisCacheService>();
+        services.AddTransient<IDatauploadService, DatauploadService>();
+
+        // Add services to the container.
+        services.AddRazorPages();
+
+        // Add Session middleware
+        services.AddDistributedMemoryCache();
+
+        services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(configuration.GetValue<int>("SessionTimeOutMinutes"));
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
+    }
+
+    public static IServiceProvider ConfigureWebApplication(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseSession();
+
+        app.MapRazorPages();
+
+        return app.Services;
+    }
+}
