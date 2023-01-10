556545a4761bfad4540df5e098650acf3c6f0836
Hardik Desai <hardik.desai@education.gov.uk>
2023-01-10 14:46:48 +0000

    Added GDS Error pages

--- src/FamilyHubs.ServiceDirectoryAdminUi.Ui/StartupExtensions.cs
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/StartupExtensions.cs
@@ -72,6 +72,7 @@ public static class StartupExtensions
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
+        app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 #if use_https
         app.UseHttpsRedirection();
