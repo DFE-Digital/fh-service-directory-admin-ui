954ede1bc900fdece89b681474e49d947b7563bb
AliShah2 <kishwer01@gmail.com>
2023-01-30 12:40:12 +0000

    Added user guid with Redis keys to make sessions unique per user

--- src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Services/RedisCacheService.cs
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Services/RedisCacheService.cs
@@ -8,6 +8,7 @@ namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.Services;
 public class RedisCacheService : IRedisCacheService
 {
     private readonly IRedisCache _redisCache;
+    private static readonly Guid _userGuid = Guid.NewGuid();
     private readonly int _timespanMinites;
 
     public RedisCacheService(IRedisCache redisCache, IConfiguration configuration)
@@ -17,68 +18,68 @@ public class RedisCacheService : IRedisCacheService
     }
     public OrganisationViewModel? RetrieveOrganisationWithService()
     {
-        return _redisCache.GetValue<OrganisationViewModel>(KeyOrgWithService);
+        return _redisCache.GetValue<OrganisationViewModel>($"{_userGuid}{KeyOrgWithService}");
     }
     public void StoreOrganisationWithService(OrganisationViewModel? vm)
     {
         if (vm != null)
-            _redisCache.SetValue(KeyOrgWithService, vm, _timespanMinites);
+            _redisCache.SetValue($"{_userGuid}{KeyOrgWithService}", vm, _timespanMinites);
     }
 
     public void ResetOrganisationWithService()
     {
-        _redisCache.SetStringValue(KeyOrgWithService, String.Empty, _timespanMinites);
+        _redisCache.SetStringValue($"{_userGuid}{KeyOrgWithService}", String.Empty, _timespanMinites);
     }
 
     public string RetrieveLastPageName()
     {
-        return _redisCache.GetStringValue(KeyCurrentPage) ?? string.Empty;
+        return _redisCache.GetStringValue($"{_userGuid}{KeyCurrentPage}") ?? string.Empty;
     }
 
     public void StoreCurrentPageName(string? currPage)
     {
         if (currPage != null)
-            _redisCache.SetStringValue(KeyCurrentPage, currPage, _timespanMinites);
+            _redisCache.SetStringValue($"{_userGuid}{KeyCurrentPage}", currPage, _timespanMinites);
     }
 
     public OpenReferralServiceDto? RetrieveService()
     {
-        return _redisCache.GetValue<OpenReferralServiceDto>(KeyService);
+        return _redisCache.GetValue<OpenReferralServiceDto>($"{_userGuid}{KeyService}");
     }
 
     public void StoreService(OpenReferralServiceDto serviceDto)
     {
-        _redisCache.SetValue(KeyService, serviceDto, _timespanMinites);
+        _redisCache.SetValue($"{_userGuid}{KeyService}", serviceDto, _timespanMinites);
     }
 
     public void StoreStringValue(string key, string value)
     {
-        _redisCache.SetStringValue(key, value);
+        _redisCache.SetStringValue($"{_userGuid}{key}", value);
     }
 
     public string RetrieveStringValue(string key)
     {
-        return _redisCache.GetStringValue(key) ?? string.Empty;
+        return _redisCache.GetStringValue($"{_userGuid}{key}") ?? string.Empty;
     }
 
     public void ResetStringValue(string key)
     {
-        _redisCache.SetStringValue(key, string.Empty);
+        _redisCache.SetStringValue($"{_userGuid}{key}", string.Empty);
     }
 
     //user flow
     public string RetrieveUserFlow()
     {
-        return _redisCache.GetStringValue(KeyUserFlow) ?? string.Empty;
+        return _redisCache.GetStringValue($"{_userGuid}{KeyUserFlow}") ?? string.Empty;
     }
 
     public void StoreUserFlow(string userFlow)
     {
-        _redisCache.SetStringValue(KeyUserFlow, userFlow, _timespanMinites);
+        _redisCache.SetStringValue($"{_userGuid}{KeyUserFlow}", userFlow, _timespanMinites);
     }
 
     public void ResetLastPageName()
     {
-        _redisCache.SetStringValue(KeyCurrentPage, String.Empty, _timespanMinites);
+        _redisCache.SetStringValue($"{_userGuid}{KeyCurrentPage}", String.Empty, _timespanMinites);
     }
 }
