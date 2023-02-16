ab324a14977ba90e87efd4e55ce9aeb7ca7656c3
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-02-16 13:25:22 +0000

    Update ServiceCollectionExtensions.cs

--- src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Extensions/ServiceCollectionExtensions.cs
+++ src/FamilyHubs.ServiceDirectoryAdminUi.Ui/Extensions/ServiceCollectionExtensions.cs
@@ -2,7 +2,6 @@
 using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services;
 using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services.Api;
 using Microsoft.Extensions.Options;
-using SFA.DAS.Http;
 
 namespace FamilyHubs.ServiceDirectoryAdminUi.Ui.Extensions;
 
@@ -10,7 +9,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddClientServices(this IServiceCollection serviceCollection)
     {
-        
+
         serviceCollection.AddClient<IApiService>((c, s) => new ApiService(c));
         serviceCollection.AddClient<IPostcodeLocationClientService>((c, s) => new PostcodeLocationClientService(c));
         serviceCollection.AddClient<ILocalOfferClientService>((c, s) => new LocalOfferClientService(c));
@@ -18,6 +17,7 @@ public static class ServiceCollectionExtensions
         serviceCollection.AddClient<IUICacheService>((c, s) => new UICacheService(c));
         serviceCollection.AddClient<ISessionService>((c, s) => new SessionService());
         serviceCollection.AddClient<ITaxonomyService>((c, s) => new TaxonomyService(c));
+
         return serviceCollection;
     }
 
@@ -25,52 +25,38 @@ public static class ServiceCollectionExtensions
         this IServiceCollection serviceCollection,
         Func<HttpClient, IServiceProvider, T> instance) where T : class
     {
-        serviceCollection.AddHttpClient<T>();
-
-        _ = serviceCollection.AddScoped(s =>
-        {
-            var client = s.GetService<HttpClient<T>>();
-            ArgumentNullException.ThrowIfNull(client, nameof(client));
-            return instance.Invoke(client.Instance, s);
-        });
-
-        return serviceCollection;
-    }
-
-    private static void AddHttpClient<T>(this IServiceCollection serviceCollection)
-    {
-
-        _ = serviceCollection.AddSingleton(serviceProvider =>
+        var name = typeof(T).Name;
+        serviceCollection.AddHttpClient(name).ConfigureHttpClient((serviceProvider, httpClient) =>
         {
             var srv = serviceProvider.GetService<IOptions<ApiOptions>>();
             ArgumentNullException.ThrowIfNull(srv, nameof(srv));
             var settings = srv.Value;
             ArgumentNullException.ThrowIfNull(settings, nameof(settings));
 
-            var clientBuilder = new HttpClientBuilder()
-                .WithDefaultHeaders()
-                .WithApimAuthorisationHeader(settings)
-                .WithLogging(serviceProvider.GetService<ILoggerFactory>());
-
-            var httpClient = clientBuilder.Build();
-
             if (!settings.ApiBaseUrl.EndsWith("/"))
             {
                 settings.ApiBaseUrl += "/";
             }
             httpClient.BaseAddress = new Uri(settings.ApiBaseUrl);
-
-            return new HttpClient<T>(httpClient);
+            httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", settings.SubscriptionKey);
+            httpClient.DefaultRequestHeaders.Add("X-Version", settings.ApiVersion);
         });
-    }
-}
 
-public class HttpClient<T>
-{
-    public HttpClient(HttpClient httpClient)
-    {
-        Instance = httpClient;
+        serviceCollection.AddScoped<T>(s =>
+        {
+            var clientFactory = s.GetService<System.Net.Http.IHttpClientFactory>();
+            var correlationService = s.GetService<ICorrelationService>();
+
+            var httpClient = clientFactory?.CreateClient(name);
+
+            ArgumentNullException.ThrowIfNull(httpClient);
+            ArgumentNullException.ThrowIfNull(correlationService);
+
+            httpClient.DefaultRequestHeaders.Add("X-Correlation-ID", correlationService.CorrelationId);
+            return instance.Invoke(httpClient, s);
+        });
+
+        return serviceCollection;
     }
 
-    public HttpClient Instance { get; private set; }
 }
