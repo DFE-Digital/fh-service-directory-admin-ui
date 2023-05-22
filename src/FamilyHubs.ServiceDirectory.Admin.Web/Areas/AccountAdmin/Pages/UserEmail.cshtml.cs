7c38e547e3129e712a0238087cca6587a7ee850a
Hardik Desai <hardik.desai@education.gov.uk>
2023-05-22 10:58:16 +0100

    email validation

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/UserEmail.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/UserEmail.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages;
@@ -11,23 +12,62 @@ public class UserEmail : AccountAdminView
         ErrorMessage = "Enter an email address";
         BackLink = "/UserName";
     }
-    
-    [BindProperty]
-    public required string EmailAddress { get; set; } = string.Empty; 
-    
+
+    [BindProperty] 
+    public required string EmailAddress { get; set; } = string.Empty;
+
     public void OnGet()
     {
-        
     }
 
     public IActionResult OnPost()
     {
-        if (ModelState.IsValid && !string.IsNullOrWhiteSpace(EmailAddress) && EmailAddress.Length <= 255 && new EmailAddressAttribute().IsValid(EmailAddress))
+        if (ModelState.IsValid && IsValidEmail(EmailAddress))
         {
             return RedirectToPage("/AddPermissionCheckAnswer");
         }
-        
+
         HasValidationError = true;
         return Page();
+    }
+
+    //https://learn.microsoft.com/en-us/dotnet/standard/base-types/how-to-verify-that-strings-are-in-valid-email-format
+    public static bool IsValidEmail(string email)
+    {
+        if (email.Length >= 255 && string.IsNullOrWhiteSpace(email))
+            return false;
+
+        try
+        {
+            // Normalize the domain
+            email = Regex.Replace(email, @"(@)(.+)$", DomainMapper, RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+            // Examines the domain part of the email and normalizes it.
+            string DomainMapper(Match match)
+            {
+                // Use IdnMapping class to convert Unicode domain names.
+                // Pull out and process domain name (throws ArgumentException on invalid)
+                var domainName = new IdnMapping().GetAscii(match.Groups[2].Value);
+
+                return match.Groups[1].Value + domainName;
+            }
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
+        catch (ArgumentException)
+        {
+            return false;
+        }
+
+        try
+        {
+            return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
     }
 }
