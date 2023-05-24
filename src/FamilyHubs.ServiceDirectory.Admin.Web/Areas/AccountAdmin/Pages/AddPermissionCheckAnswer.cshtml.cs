0b6e0b4054a1a8ad8761f7434bd7cfde8a2b828b
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-05-24 15:05:53 +0100

    Check Details Page

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/AddPermissionCheckAnswer.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/AddPermissionCheckAnswer.cshtml.cs
@@ -4,8 +4,23 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages;
 
 public class AddPermissionCheckAnswer : PageModel
 {
+    public string WhoFor { get; set; } = string.Empty;
+    public string TypeOfPermission { get; set; } = string.Empty;
+    public string LocalAuthority { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+
     public void OnGet()
     {
-        
+        PopulateAccountDetails();
+    }
+
+    private void PopulateAccountDetails()
+    {
+        WhoFor = "Someone who works for a local authority";
+        TypeOfPermission = "Add and manage services, family hubs and accounts";
+        LocalAuthority = "Bristol";
+        Email = "robert.warpole@firstpm.com";
+        Name = "Robert Warpole";
     }
 }
---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/CheckDetailsRow.cs
@@ -0,0 +1,16 @@
+﻿namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages
+{
+    public class CheckDetailsRow
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string ChangeLocation { get; set; }
+
+        public CheckDetailsRow(string name, string value, string changeLocation)
+        {
+            Name = name;
+            Value = value;
+            ChangeLocation = changeLocation;
+        }
+    }
+}
