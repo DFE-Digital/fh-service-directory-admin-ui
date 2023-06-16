b8a22c611b5a1fe2e8d4b3f261130ce4813f6fab
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-06-16 09:06:24 +0100

    Content added to confirmation page

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/Confirmation.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/Confirmation.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
+using FamilyHubs.ServiceDirectory.Admin.Core.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages
 {
     public class ConfirmationModel : PageModel
     {
-        public void OnGet()
+        private readonly ICacheService _cacheService;
+        public string Name { get; set; } = string.Empty;
+
+        public ConfirmationModel(ICacheService cacheService)
         {
+            _cacheService = cacheService;
+        }
+
+        public async Task OnGet()
+        {
+            var cachedModel = await _cacheService.GetPermissionModel();
+            ArgumentNullException.ThrowIfNull(cachedModel);
+            Name = cachedModel.FullName;
         }
     }
 }
