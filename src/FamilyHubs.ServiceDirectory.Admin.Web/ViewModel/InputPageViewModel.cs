e6c6dcad940a43589b783c32c630b7f06602cbf2
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-06-20 16:22:05 +0100

    Fixed to work in appservice

--- src/FamilyHubs.ServiceDirectory.Admin.Web/ViewModel/InputPageViewModel.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/ViewModel/InputPageViewModel.cs
@@ -24,15 +24,33 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.ViewModel
                 return;
             }
 
-            var host = HttpContext.Request.Host.Value;
+            var host = GetHost();
             var path = HttpContext.Request.Headers.Referer;
 
-            if(path.IsNullOrEmpty() || string.IsNullOrEmpty(host))
+            if(path.IsNullOrEmpty())
             {
-                return;
+                throw new Exception("Request does not contain a path");
             }
 
             BackButtonPath = path.ToString().Substring(path.ToString().IndexOf(host) + host.Length);
         }
+
+        private string GetHost()
+        {
+            var host = HttpContext.Request.Host.Value;
+
+            if (HttpContext.Request.Headers.ContainsKey("X-Forwarded-Host"))
+            {
+                var xforwardedhost = HttpContext.Request.Headers["X-Forwarded-Host"];
+                if (!xforwardedhost.IsNullOrEmpty())
+                {
+                    host = xforwardedhost.ToString();
+                }
+            }
+
+            return host;
+            
+        }
+
     }
 }
