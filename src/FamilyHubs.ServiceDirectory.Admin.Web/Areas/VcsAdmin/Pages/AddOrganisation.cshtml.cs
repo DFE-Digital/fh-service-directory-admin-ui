059daf3ecc468e2ef389e171653070a6394df12a
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-06-20 15:16:05 +0100

    Back button and post

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/VcsAdmin/Pages/AddOrganisation.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/VcsAdmin/Pages/AddOrganisation.cshtml.cs
@@ -1,12 +1,37 @@
+using FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages
 {
-    public class AddOrganisationModel : PageModel
+    public class AddOrganisationModel : InputPageViewModel
     {
+        [BindProperty]
+        public string OrganisationName { get; set; } = string.Empty;
+
+        public AddOrganisationModel()
+        {
+            PageHeading = "What is the organisations name?";
+            ErrorMessage = "Enter the organisation's name";
+            ContinuePath = "/AddOrganisationCheckDetails";
+        }
+
         public void OnGet()
         {
+            SetBackButtonPath();
+        }
+
+        public IActionResult OnPost()
+        {
+            SetBackButtonPath();
+
+            if (ModelState.IsValid && !string.IsNullOrWhiteSpace(OrganisationName) && OrganisationName.Length <= 255)
+            {
+                return RedirectToPage(ContinuePath);
+            }
+
+            HasValidationError = true;
+
+            return Page();
         }
     }
 }
---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/ViewModel/InputPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.ViewModel
+{
+    public class InputPageViewModel : PageModel
+    {
+        [BindProperty]
+        public string BackButtonPath { get; set; } = string.Empty;
+        public string ContinuePath { get; set; } = string.Empty;
+        public bool HasValidationError { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
+        public string ErrorElementId { get; set; } = string.Empty;
+        public string PageHeading { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Sets the back button path using HttpContext
+        /// </summary>
+        protected void SetBackButtonPath()
+        {
+            if (!string.IsNullOrEmpty(BackButtonPath))
+            {
+                return;
+            }
+
+            var host = HttpContext.Request.Host.Value;
+            var path = HttpContext.Request.Headers.Referer;
+
+            if(path.IsNullOrEmpty() || string.IsNullOrEmpty(host))
+            {
+                return;
+            }
+
+            BackButtonPath = path.ToString().Substring(path.ToString().IndexOf(host) + host.Length);
+        }
+    }
+}
