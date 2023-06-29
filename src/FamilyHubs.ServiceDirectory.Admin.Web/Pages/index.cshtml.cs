679d567daba6694f95c66168aa69f9dcb2addf83
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-06-29 11:11:07 +0100

    UI Fixes

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using FamilyHubs.SharedKernel.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public IActionResult OnGet()
+    {
+        if (HttpContext.IsUserLoggedIn())
+        {
+            return RedirectToPage("/Welcome");
+        }
+
+        return Page();
+    }
+}
