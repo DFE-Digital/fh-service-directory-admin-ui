ad06bb06e74857a47bba690d8af593748945be5c
Witek Poreda <witek.poreda@education.gov.uk>
2023-07-04 12:01:55 +0100

    #FGH-2101 signout page added

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/SignOut.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages
+{
+    public class SignOutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }      
+    }
+}
