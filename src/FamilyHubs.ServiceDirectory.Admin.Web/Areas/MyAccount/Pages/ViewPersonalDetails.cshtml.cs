b48dd94808d7e15878c27092ae9f3f6e38edb0b2
Witek Poreda <witek.poreda@education.gov.uk>
2023-07-04 11:57:16 +0100

    #FGH-2101 #FHG-2106 My account area added with view personal details page

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/MyAccount/Pages/ViewPersonalDetails.cshtml.cs
@@ -0,0 +1,24 @@
+using FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
+using FamilyHubs.SharedKernel.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.MyAccount.Pages
+{
+    public class ViewPersonalDetails : MyAccountViewModel
+    {
+
+        public string FullName { get; set; }
+
+        public ViewPersonalDetails(IConfiguration configuration)
+        {
+            PreviousPageLink = "/Welcome";
+            GovOneLoginAccountPage = configuration.GetValue<string>("GovUkOidcConfiguration:Oidc:BaseUrl") + "/security";
+        }
+
+        public void OnGet()
+        {
+            FullName = HttpContext.GetFamilyHubsUser().FullName;
+        }
+    }
+}
