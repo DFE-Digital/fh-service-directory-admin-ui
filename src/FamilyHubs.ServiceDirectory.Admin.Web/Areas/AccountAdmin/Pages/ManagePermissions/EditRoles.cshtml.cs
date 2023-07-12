6ee5bbb6bc18dd39d6bdd66e29b2498975ede40e
Witek Poreda <witek.poreda@education.gov.uk>
2023-07-12 09:28:20 +0100

    Edit roles UI

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/ManagePermissions/EditRoles.cshtml.cs
@@ -0,0 +1,154 @@
+using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
+using FamilyHubs.ServiceDirectory.Admin.Core.Helpers;
+using FamilyHubs.ServiceDirectory.Admin.Core.Models;
+using FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
+using FamilyHubs.SharedKernel.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Mail;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages.ManagePermissions
+{
+    public class EditRolesModel : InputPageViewModel
+    {
+        private readonly IIdamClient _idamClient;
+
+        [BindProperty(SupportsGet = true)]
+        public string AccountId { get; set; } = string.Empty; //Route Property               
+
+        [BindProperty]
+        public bool LaProfessional { get; set; }
+
+        [BindProperty]
+        public bool LaManager { get; set; }
+
+        [BindProperty]
+        public bool VcsProfessional { get; set; }
+
+        [BindProperty]
+        public bool VcsManager { get; set; }
+
+        
+        public bool IsLa { get; set; } = false;
+        
+        
+        public bool IsVcs { get; set; } = false;
+
+        public EditRolesModel(IIdamClient idamClient)
+        {
+            PageHeading = "What do they need to do?";
+            ErrorMessage = "Select what they need to do";
+            SubmitButtonText = "Confirm";            
+            _idamClient = idamClient;
+        }
+
+        public async Task OnGet()
+        {
+            BackButtonPath = $"/AccountAdmin/ManagePermissions/{AccountId}";
+
+            var id = GetAccountId();
+            var account = await GetAccount(id);
+            string role = GetRole(account);
+            SetOrganisationType(role);
+            SetRoleSelection(role);
+        }
+
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid && (LaManager || LaProfessional || VcsManager || VcsProfessional))
+            {
+                //update roles api call 
+                return RedirectToPage("/placeholder");
+            }
+
+            HasValidationError = true;
+
+            var id = GetAccountId();
+            var account = await GetAccount(id);
+            string role = GetRole(account);
+            SetOrganisationType(role);
+
+            return Page();
+        }
+
+        private void SetOrganisationType(string role)
+        {
+            if (role == RoleTypes.LaManager || role == RoleTypes.LaProfessional ||role == RoleTypes.LaDualRole)
+            {
+                IsLa = true;
+            }
+            //Vcs
+            else if (role == RoleTypes.VcsManager || role == RoleTypes.VcsProfessional || role == RoleTypes.VcsDualRole)
+            {
+               IsVcs = true;
+            }
+        }
+
+        private void SetRoleSelection(string role)
+        {
+            //La
+            if (role == RoleTypes.LaManager)
+            {
+                LaManager = true;                
+            }
+            else if (role == RoleTypes.LaProfessional)
+            {
+                LaProfessional = true;                
+            }
+            else if (role == RoleTypes.LaDualRole)
+            {
+                LaManager = true;
+                LaProfessional = true;                
+            }
+            //Vcs
+            else if (role == RoleTypes.VcsManager)
+            {
+                VcsManager = true;                
+            }
+            else if (role == RoleTypes.VcsProfessional)
+            {
+                VcsProfessional = true;                
+            }
+            else if (role == RoleTypes.VcsDualRole)
+            {                
+                VcsManager = true;
+                VcsProfessional = true;
+            }
+        }
+
+        private long GetAccountId()
+        {
+            if (long.TryParse(AccountId, out long id))
+            {
+                return id;
+            }
+
+            throw new Exception("Invalid AccountId");
+        }
+
+        private async Task<AccountDto?> GetAccount(long id)
+        {
+            var account = await _idamClient.GetAccountById(id);
+
+            if (account is not null)
+            {
+                return account;
+            }
+
+            throw new Exception("User not found");
+        }
+
+        private string GetRole(AccountDto? account)
+        {
+            if (account is not null)
+            {
+                var roleClaim = account.Claims.Where(x => x.Name == FamilyHubsClaimTypes.Role).Single();
+                var role = roleClaim.Value;
+                return role;
+            }
+
+            throw new Exception("Role not found");
+        }
+    }
+}
