d736e3a8a170d0976982faa7ee4bdf976423f604
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2023-07-17 09:28:56 +0100

    FHG-3452 No Claims 401 (#180)
    Co-authored-by: Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Error/401.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.Error
+{
+    public class _401Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
