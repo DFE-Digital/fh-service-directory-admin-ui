162a359a0ac570362cec5f923cb28f2cd6056fcc
witpo <witek.poreda@digital.education.gov.uk>
2023-07-18 13:04:20 +0100

    Fhg 3717 manage permission delete user (#179)
    * Delete user page added
    
    * Delete all claims added to Idam Client
    
    * delete user and claims from db
    
    * confirmation page added
    
    * unit test added for DeleteUser
    
    * tests added for Delete User Confirmation
    
    ---------
    
    Co-authored-by: Witek Poreda <witek.poreda@education.gov.uk>

---
+++ Test/FamilyHubs.ServiceDirectory.Admin.Web.UnitTests/Areas/AccountAdmin/ManagePermissions/DeleteUserConfirmationTests.cs
@@ -0,0 +1,35 @@
+﻿using FamilyHubs.ServiceDirectory.Admin.Core.Services;
+using FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages.ManagePermissions;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.UnitTests.Areas.AccountAdmin.ManagePermissions
+{
+    public class DeleteUserConfirmationTests
+    {
+        
+        private readonly Mock<ICacheService> _mockCacheService;
+        public DeleteUserConfirmationTests()
+        {            
+            _mockCacheService = new Mock<ICacheService>();
+        }
+            
+        [Fact]
+        public async Task OnGet_UserNameRetrivedFromCache()
+        {
+            //  Arrange            
+            const string userName = "TestUser";
+            const bool isDeleted = true;
+            _mockCacheService.Setup(x=>x.RetrieveString(It.IsAny<string>())).Returns(Task.FromResult(userName));
+            var sut = new DeleteUserConfirmationModel( _mockCacheService.Object);
+
+            //  Act
+            await sut.OnGet(isDeleted);
+
+            //  Assert
+            _mockCacheService.Verify(x => x.RetrieveString("DeleteUserName"), Times.Once());
+            Assert.Equal(userName, sut.UserName);
+        }               
+    }
+}
---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/ManagePermissions/DeleteUser.cshtml.cs
@@ -0,0 +1,76 @@
+using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
+using FamilyHubs.ServiceDirectory.Admin.Core.Services;
+using FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages.ManagePermissions
+{
+    public class DeleteUserModel : InputPageViewModel
+    {
+        private readonly IIdamClient _idamClient;
+        private readonly ICacheService _cacheService;
+
+        [BindProperty]
+        [Required]
+        public bool? DeleteUser { get; set; } = null;
+        public string UserName { get; set; } = string.Empty;       
+
+        public DeleteUserModel(IIdamClient idamClient, ICacheService cacheService)
+        {
+            ErrorMessage = "Select if you want to delete the account";
+            SubmitButtonText = "Confirm";                       
+            _idamClient = idamClient;
+            _cacheService = cacheService;
+        }
+
+        public async Task OnGet(long accountId)
+        {
+            BackButtonPath = await _cacheService.RetrieveLastPageName();
+            
+            var account = await _idamClient.GetAccountById(accountId);
+
+            if (account == null)
+            {
+                throw new Exception("User Account not found");
+            }
+
+            UserName = account.Name;
+            await _cacheService.StoreString("DeleteUserName", UserName);
+
+            PageHeading = $"Do you want to delete {UserName}'s permissions?";
+            HintText = $"This will remove all permissions that have been given to {UserName}.";
+        }
+
+
+        public async Task<IActionResult> OnPost(long accountId)
+        {
+            if (ModelState.IsValid)
+            {
+                if ( DeleteUser is not null)
+                {
+                    if ( DeleteUser.Value == true )
+                    {
+                        await _idamClient.DeleteAccount(accountId);
+                        return RedirectToPage("DeleteUserConfirmation", new { AccountId = accountId, IsDeleted = true });
+                    }
+                    else
+                    {
+                        return RedirectToPage("DeleteUserConfirmation", new { AccountId = accountId, IsDeleted = false });
+                    }
+                }
+                
+                
+            }
+            UserName = await _cacheService.RetrieveString("DeleteUserName");
+            PageHeading = $"Do you want to delete {UserName}'s permissions?";
+            HintText = $"This will remove all permissions that have been given to {UserName}.";
+
+            HasValidationError = true;
+            return Page();
+        }
+    }
+}
