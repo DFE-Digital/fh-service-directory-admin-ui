87204989634fb2ffaf134427e0bf588bad67959b
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2023-07-19 11:40:53 +0100

    FHG-3848 and FHG-3849 (#187)
    Co-authored-by: Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/VcsAdmin/Pages/AddOrganisationWhichLocalAuthority.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/VcsAdmin/Pages/AddOrganisationWhichLocalAuthority.cshtml.cs
@@ -1,10 +1,7 @@
 using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
-using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Core.Services;
-using FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages;
 using FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages
 {
