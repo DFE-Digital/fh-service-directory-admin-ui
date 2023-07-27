023021f9073e4aad79c4f587a271b314f966dd7c
witpo <witek.poreda@digital.education.gov.uk>
2023-07-27 14:29:56 +0100

    Fhg 3825 la permission change notification (#206)
    * LA user permission change emails added
    
    * permission change VCS notifications added
    
    * fix exisitng tests, new tests for send emial
    
    * template ids moved to a separate file
    
    ---------
    
    Co-authored-by: Witek Poreda <witek.poreda@education.gov.uk>

--- src/FamilyHubs.ServiceDirectory.Admin.Core/Services/EmailService.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Core/Services/EmailService.cs
@@ -1,28 +1,23 @@
 ﻿using FamilyHubs.ServiceDirectory.Admin.Core.Models;
+using FamilyHubs.SharedKernel.Identity;
 using Microsoft.Extensions.Logging;
 using Notify.Interfaces;
+using System.Security;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Core.Services;
 
 public interface IEmailService
 {
     Task SendAccountPermissionAddedEmail(PermissionModel model);
+    Task SendLaPermissionChangeEmail(PermissionChangeNotificationModel notification);
+    Task SendVcsPermissionChangeEmail(PermissionChangeNotificationModel notification);
 }
 
 public class EmailService : IEmailService
 {
     private readonly IAsyncNotificationClient _notificationClient;
     private readonly ILogger<EmailService> _logger;
-
-    private const string LaDualRoleEmailTemplateId = "eee5cb96-8387-4095-a942-dfe4885b4db3";
-    private const string LaManagerEmailTemplateId = "cc0ba892-c9ae-4990-a07d-f38c4062fd59";
-    private const string LaProfessionalEmailTemplateId = "5074a730-74bc-42fd-ad5b-d1100d7f11ca";
-    
-    private const string VcsDualRoleEmailTemplateId = "74acfbed-428e-49d6-b35c-9b6279b4b2ee";
-    private const string VcsManagerEmailTemplateId = "2bfb0fdd-374f-478f-b842-973466a96efe";
-    private const string VcsProfessionalEmailTemplateId = "6d5e73b8-5db8-497e-892f-6edd7e5506ec";
-
-
+  
     public EmailService(IAsyncNotificationClient notificationClient, ILogger<EmailService> logger)
     {
         _notificationClient = notificationClient;
@@ -35,7 +30,7 @@ public class EmailService : IEmailService
         {
             // TODO: Replace new Dictionary<string, dynamic>() with new keyvaluepair
             await _notificationClient.SendEmailAsync(model.EmailAddress, GetEmailTemplateId(model), new Dictionary<string, dynamic>());
-            
+
             _logger.LogInformation("Account Permission Added Email Sent");
         }
         catch (Exception e)
@@ -49,34 +44,143 @@ public class EmailService : IEmailService
     {
         if (model.LaDualRole)
         {
-            return LaDualRoleEmailTemplateId;
+            return EmailTempaltes.LaPermissionAddedDualRoleEmailTemplateId;
         }
 
         if (model.LaManager)
         {
-            return LaManagerEmailTemplateId;
+            return EmailTempaltes.LaPermissionAddedManagerEmailTemplateId;
         }
-        
+
         if (model.LaProfessional)
         {
-            return LaProfessionalEmailTemplateId;
+            return EmailTempaltes.LaPermissionAddedProfessionalEmailTemplateId;
         }
-        
+
         if (model.VcsDualRole)
         {
-            return VcsDualRoleEmailTemplateId;
+            return EmailTempaltes.VcsPermissionAddedDualRoleEmailTemplateId;
         }
 
         if (model.VcsManager)
         {
-            return VcsManagerEmailTemplateId;
+            return EmailTempaltes.VcsPermissionAddedManagerEmailTemplateId;
         }
-        
+
         if (model.VcsProfessional)
         {
-            return VcsProfessionalEmailTemplateId;
+            return EmailTempaltes.VcsPermissionAddedProfessionalEmailTemplateId;
         }
 
         throw new InvalidOperationException("Valid role not find in account permission view model, unable to send confirmation email");
     }
+
+
+    public async Task SendLaPermissionChangeEmail(PermissionChangeNotificationModel notification)
+    {
+        try
+        {
+            var templateId = GetLaPermissionChangeTemplateId(notification.OldRole, notification.NewRole);
+            // TODO: Replace new Dictionary<string, dynamic>() with new keyvaluepair
+            await _notificationClient.SendEmailAsync(notification.EmailAddress, templateId, new Dictionary<string, dynamic>());
+
+            _logger.LogInformation("Account Permission Modified Email template {templateId} Sent", templateId);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error sending Account Permission Modified Email LA");
+            throw;
+        }
+    }
+
+
+    
+
+    private string GetLaPermissionChangeTemplateId(string oldRole, string newRole)
+    {
+        if (oldRole == RoleTypes.LaManager && newRole == RoleTypes.LaProfessional)
+        {
+            return EmailTempaltes.LaPermissionChangedFromLaManagerToLaProfessional;
+        }
+        
+        if (oldRole == RoleTypes.LaProfessional && newRole == RoleTypes.LaManager)
+        {
+            return EmailTempaltes.LaPermissionChangedFromLaProfessionalToLaManager;
+        }
+        
+        if (oldRole == RoleTypes.LaManager && newRole == RoleTypes.LaDualRole)
+        {
+            return EmailTempaltes.LaPermissionChangedFromLaManagerToLaDualRole;
+        }
+        
+        if (oldRole == RoleTypes.LaProfessional && newRole == RoleTypes.LaDualRole)
+        {
+            return EmailTempaltes.LaPermissionChangedFromLaProfessionalToLaDualRole;
+        }
+        
+        if (oldRole == RoleTypes.LaDualRole && newRole == RoleTypes.LaManager)
+        {
+            return EmailTempaltes.LaPermissionChangedFromLaDualRoleToLaManager;
+        }
+        
+        if (oldRole == RoleTypes.LaDualRole && newRole == RoleTypes.LaProfessional)
+        {
+            return EmailTempaltes.LaPermissionChangedFromLaDualRoleToLaProfessional;
+        }
+
+        throw new InvalidOperationException("Valid email template not found in Permission Change Notification Model, unable to send confirmation email for LA");
+    }
+
+    public async Task SendVcsPermissionChangeEmail(PermissionChangeNotificationModel notification)
+    {
+        try
+        {
+            var templateId = GetVcsPermissionChangeTemplateId(notification.OldRole, notification.NewRole);
+            // TODO: Replace new Dictionary<string, dynamic>() with new keyvaluepair
+            await _notificationClient.SendEmailAsync(notification.EmailAddress, templateId, new Dictionary<string, dynamic>());
+
+            _logger.LogInformation("Account Permission Modified Email template {templateId} Sent", templateId);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error sending Account Permission Modified Email VCS");
+            throw;
+        }
+    }
+
+    private string GetVcsPermissionChangeTemplateId(string oldRole, string newRole)
+    {
+        if (oldRole == RoleTypes.VcsManager && newRole == RoleTypes.VcsProfessional)
+        {
+            return EmailTempaltes.VcsPermissionChangedFromVcsManagerToVcsProfessional;
+        }
+        
+        if (oldRole == RoleTypes.VcsProfessional && newRole == RoleTypes.VcsManager)
+        {
+            return EmailTempaltes.VcsPermissionChangedFromVcsProfessionalToVcsManager;
+        }
+        
+        if (oldRole == RoleTypes.VcsManager && newRole == RoleTypes.VcsDualRole)
+        {
+            return EmailTempaltes.VcsPermissionChangedFromVcsManagerToVcsDualRole;
+        }
+        
+        if (oldRole == RoleTypes.VcsProfessional && newRole == RoleTypes.VcsDualRole)
+        {
+            return EmailTempaltes.VcsPermissionChangedFromVcsProfessionalToVcsDualRole;
+        }
+        
+        if (oldRole == RoleTypes.VcsDualRole && newRole == RoleTypes.VcsManager)
+        {
+            return EmailTempaltes.VcsPermissionChangedFromVcsDualRoleToVcsManager;
+        }
+        
+        if (oldRole == RoleTypes.VcsDualRole && newRole == RoleTypes.VcsProfessional)
+        {
+            return EmailTempaltes.VcsPermissionChangedFromVcsDualRoleToVcsProfessional;
+        }
+
+        throw new InvalidOperationException("Valid email template not found in Permission Change Notification Model, unable to send confirmation email for VCS");
+    }
 }
+
--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/ManagePermissions/EditRoles.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/AccountAdmin/Pages/ManagePermissions/EditRoles.cshtml.cs
@@ -1,20 +1,16 @@
 using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
-using FamilyHubs.ServiceDirectory.Admin.Core.Helpers;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
+using FamilyHubs.ServiceDirectory.Admin.Core.Services;
 using FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
 using FamilyHubs.SharedKernel.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Net.Mail;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages.ManagePermissions
 {
     public class EditRolesModel : InputPageViewModel
     {
         private readonly IIdamClient _idamClient;
-
-        [BindProperty(SupportsGet = true)]
-        public string AccountId { get; set; } = string.Empty; //Route Property               
+        private readonly IEmailService _emailService;
 
         [BindProperty]
         public bool LaProfessional { get; set; } = false;
@@ -27,50 +23,61 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages.ManageP
 
         [BindProperty]
         public bool VcsManager { get; set; } = false;
-
-
+      
         public bool IsLa { get; set; } = false;
-
-
+        
         public bool IsVcs { get; set; } = false;
 
-        public EditRolesModel(IIdamClient idamClient)
+        public EditRolesModel(IIdamClient idamClient, IEmailService emailService)
         {
             PageHeading = "What do they need to do?";
             ErrorMessage = "Select what they need to do";
             SubmitButtonText = "Confirm";
             _idamClient = idamClient;
+            _emailService = emailService;
         }
 
-        public async Task OnGet()
+        public async Task OnGet(long accountId)
         {
-            BackButtonPath = $"/AccountAdmin/ManagePermissions/{AccountId}";
-
-            var id = GetAccountId();
-            var account = await GetAccount(id);
+            BackButtonPath = $"/AccountAdmin/ManagePermissions/{accountId}";
+            
+            var account = await GetAccount(accountId);
             string role = GetRole(account);
             SetOrganisationType(role);
             SetRoleSelection(role);
         }
 
 
-        public async Task<IActionResult> OnPost()
+        public async Task<IActionResult> OnPost(long accountId)
         {
+            var account = await GetAccount(accountId);
+
             if (ModelState.IsValid && (LaManager || LaProfessional || VcsManager || VcsProfessional))
             {
-                var accountId = GetAccountId();
-
+                var oldRole = GetRole(account);
                 var newRole = GetSelectedRole();
+                SetOrganisationType(newRole);
                 var request = new UpdateClaimDto { AccountId = accountId, Name = "role", Value = newRole };
                 await _idamClient.UpdateClaim(request);
 
-                return RedirectToPage("EditRolesChangedConfirmation", new { AccountId = AccountId });
+                var email = new PermissionChangeNotificationModel() {
+                    EmailAddress = account.Email, 
+                    OldRole = oldRole,
+                    NewRole = newRole
+                };
+                if (IsLa)
+                {
+                    await _emailService.SendLaPermissionChangeEmail(email);
+                }else if ( IsVcs)
+                {
+                    await _emailService.SendVcsPermissionChangeEmail(email);
+                }
+                
+                return RedirectToPage("EditRolesChangedConfirmation", new { AccountId = accountId });
             }
 
             HasValidationError = true;
-
-            var id = GetAccountId();
-            var account = await GetAccount(id);
+                        
             string role = GetRole(account);
             SetOrganisationType(role);
 
@@ -156,17 +163,7 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.AccountAdmin.Pages.ManageP
                 VcsProfessional = true;
             }
         }
-
-        private long GetAccountId()
-        {
-            if (long.TryParse(AccountId, out long id))
-            {
-                return id;
-            }
-
-            throw new Exception("Invalid AccountId");
-        }
-
+    
         private async Task<AccountDto?> GetAccount(long id)
         {
             var account = await _idamClient.GetAccountById(id);
