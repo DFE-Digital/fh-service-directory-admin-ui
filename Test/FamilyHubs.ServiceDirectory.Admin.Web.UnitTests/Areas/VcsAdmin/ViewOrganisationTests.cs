e2a97e99f934baba122d81c42b73d322585f88e3
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-07-31 09:58:28 +0100

    FHG-3799 ViewOrganisation

---
+++ Test/FamilyHubs.ServiceDirectory.Admin.Web.UnitTests/Areas/VcsAdmin/ViewOrganisationTests.cs
@@ -0,0 +1,124 @@
+﻿using AutoFixture;
+using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
+using FamilyHubs.ServiceDirectory.Admin.Core.Services;
+using FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages;
+using FamilyHubs.ServiceDirectory.Shared.Dto;
+using FamilyHubs.ServiceDirectory.Shared.Enums;
+using FamilyHubs.SharedKernel.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.UnitTests.Areas.VcsAdmin
+{
+    public class ViewOrganisationTests
+    {
+        private readonly Mock<IServiceDirectoryClient> _mockServiceDirectoryClient;
+        private readonly Mock<ICacheService> _mockCacheService;
+        private readonly Mock<ILogger<ViewOrganisationModel>> _mockLogger;
+        private readonly Fixture _fixture;
+
+        public ViewOrganisationTests()
+        {
+            _mockServiceDirectoryClient = new Mock<IServiceDirectoryClient>();
+            _mockCacheService= new Mock<ICacheService>();
+            _mockLogger = new Mock<ILogger<ViewOrganisationModel>>();
+            _fixture = new Fixture();
+            ConfigureMockServiceClient();
+        }
+
+        [Fact]
+        public async Task OnGet_ReturnsPage()
+        {
+            //  Arrange
+            var mockHttpContext = GetHttpContext(RoleTypes.DfeAdmin, -1);
+            var sut = new ViewOrganisationModel(_mockServiceDirectoryClient.Object, _mockCacheService.Object, _mockLogger.Object)
+            {
+                PageContext = { HttpContext = mockHttpContext.Object },
+                OrganisationId = "2"
+            };
+
+            //  Act
+            var response = await sut.OnGet();
+
+            //  Assert
+            Assert.IsType<PageResult>(response);
+        }
+
+        [Theory]
+        [InlineData("5", "Organisation 5 not found")]
+        [InlineData("1", "Organisation 1 is not a VCS organisation")]
+        [InlineData("3", "Organisation 3 has no parent")]
+        [InlineData("4", "User testuser@test.com cannot view 4")]
+        public async Task OnGet_InvalidOrganisation_RedirectsToError(string organisationId, string expectedLogMessage)
+        {
+            //  Arrange
+            var mockHttpContext = GetHttpContext(RoleTypes.LaManager, 1);
+            var sut = new ViewOrganisationModel(_mockServiceDirectoryClient.Object, _mockCacheService.Object, _mockLogger.Object)
+            {
+                PageContext = { HttpContext = mockHttpContext.Object },
+                OrganisationId = organisationId
+            };
+
+            //  Act
+            var response = await sut.OnGet();
+
+            //  Assert
+            Assert.IsType<RedirectToPageResult>(response);
+            AssertLoggerWarning(expectedLogMessage);
+        }
+
+        private Mock<HttpContext> GetHttpContext(string role, long organisationId)
+        {
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Email, "testuser@test.com") ,
+                new Claim(FamilyHubsClaimTypes.FullName, "any") ,
+                new Claim(FamilyHubsClaimTypes.AccountId, "1"),
+                new Claim(FamilyHubsClaimTypes.OrganisationId, organisationId.ToString()),
+                new Claim(FamilyHubsClaimTypes.Role, role),
+            };
+
+            return TestHelper.GetHttpContext(claims);
+        }
+
+        private void ConfigureMockServiceClient()
+        {
+            var la = TestHelper.CreateTestOrganisationWithServices(1, null, OrganisationType.LA, _fixture);
+            var laResponse = Task.FromResult((OrganisationWithServicesDto?)la);
+
+            var vcs = TestHelper.CreateTestOrganisationWithServices(2, 1, OrganisationType.VCFS, _fixture);
+            var vcsResponse = Task.FromResult((OrganisationWithServicesDto?)vcs);
+
+            var vcsNoParent = TestHelper.CreateTestOrganisationWithServices(3, null, OrganisationType.VCFS, _fixture);
+            var vcsNoParentResponse = Task.FromResult((OrganisationWithServicesDto?)vcsNoParent);
+
+            var vcsUnauthorisedUser = TestHelper.CreateTestOrganisationWithServices(4, 99, OrganisationType.VCFS, _fixture);
+            var vcsUnauthorisedUserResponse = Task.FromResult((OrganisationWithServicesDto?)vcsUnauthorisedUser);
+
+            _mockServiceDirectoryClient.Setup(x => x.GetOrganisationById(It.Is<long>(x => x == 1))).Returns(laResponse);
+            _mockServiceDirectoryClient.Setup(x => x.GetOrganisationById(It.Is<long>(x => x == 2))).Returns(vcsResponse);
+            _mockServiceDirectoryClient.Setup(x => x.GetOrganisationById(It.Is<long>(x => x == 3))).Returns(vcsNoParentResponse);
+            _mockServiceDirectoryClient.Setup(x => x.GetOrganisationById(It.Is<long>(x => x == 4))).Returns(vcsUnauthorisedUserResponse);
+        }
+
+        private void AssertLoggerWarning(string message)
+        {
+            _mockLogger.Verify(logger => logger.Log(
+                It.Is<LogLevel>(logLevel => logLevel == LogLevel.Warning),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((@object, @type) => @object.ToString() == message && @type.Name == "FormattedLogValues"),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        }
+    }
+}
