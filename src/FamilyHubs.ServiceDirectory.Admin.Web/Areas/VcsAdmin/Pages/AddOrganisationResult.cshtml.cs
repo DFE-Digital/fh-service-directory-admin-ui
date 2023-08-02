61a7be00c6d46680740831379e860c35ef9072fa
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-08-02 15:43:22 +0100

    FHG-3930 Add organisation results screen and authorization

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/VcsAdmin/Pages/AddOrganisationResult.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/VcsAdmin/Pages/AddOrganisationResult.cshtml.cs
@@ -1,3 +1,4 @@
+using FamilyHubs.SharedKernel.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,22 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages
 {
     public class AddOrganisationResultModel : PageModel
     {
+        public string DisplayMessage { get; set; } = string.Empty;
+
         public void OnGet()
         {
+            var user = HttpContext.GetFamilyHubsUser();
+            switch (user.Role)
+            {
+                case RoleTypes.DfeAdmin:
+                    DisplayMessage = "You can now create user accounts for the organisation. You can also add its services to the directory.";
+                    break;
+
+                case RoleTypes.LaDualRole:
+                case RoleTypes.LaManager:
+                    DisplayMessage = "You can now create user accounts for the organisation.";
+                    break;
+            }
         }
     }
 }
