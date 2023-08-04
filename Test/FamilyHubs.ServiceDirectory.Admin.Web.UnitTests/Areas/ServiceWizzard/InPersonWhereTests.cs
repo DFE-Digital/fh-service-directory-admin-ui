92c0f03e873b73cc62cb2efe7a02037e0223b26f
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-08-04 15:01:09 +0100

    Added additional scenarios To InPersonWhere Tests

--- Test/FamilyHubs.ServiceDirectory.Admin.Web.UnitTests/Areas/ServiceWizzard/InPersonWhereTests.cs
+++ Test/FamilyHubs.ServiceDirectory.Admin.Web.UnitTests/Areas/ServiceWizzard/InPersonWhereTests.cs
@@ -18,22 +18,16 @@ public class InPersonWhereTests
 {
     private readonly Mock<IRequestDistributedCache> _mockRequestDistributedCache;
     private readonly InPersonWhereModel _inPersonWhere;
+    private readonly Mock<IPostcodeLocationClientService> _mockPostcodeLocationClientService;
 
     public InPersonWhereTests()
     {
-        Mock<IPostcodeLocationClientService> mockPostcodeLocationClientService = new Mock<IPostcodeLocationClientService>();
+        _mockPostcodeLocationClientService = new Mock<IPostcodeLocationClientService>();
 
-        var response = new PostcodesIoResponse();
-        response.Result = new PostcodeInfo
-        {
-            Longitude = 1.0,
-            Latitude = 2.0,
-        };
-
-        mockPostcodeLocationClientService.Setup(x => x.LookupPostcode(It.IsAny<string>())).ReturnsAsync(response);
+        
         _mockRequestDistributedCache = new Mock<IRequestDistributedCache>();
         DefaultHttpContext httpContext = new DefaultHttpContext();
-        _inPersonWhere = new InPersonWhereModel(_mockRequestDistributedCache.Object, mockPostcodeLocationClientService.Object);
+        _inPersonWhere = new InPersonWhereModel(_mockRequestDistributedCache.Object, _mockPostcodeLocationClientService.Object);
         _inPersonWhere.PageContext.HttpContext = httpContext;
         CompiledPageActionDescriptor compiledPageActionDescriptor = new();
         _inPersonWhere.PageContext.ActionDescriptor = compiledPageActionDescriptor;
@@ -82,6 +76,47 @@ public class InPersonWhereTests
     }
 
     [Fact]
+    public async Task ThenServiceDeliveryTypeOnGetIsSuccessfull_WithSingleAddressLine()
+    {
+        //Arrange
+        _mockRequestDistributedCache.Setup(x => x.GetAsync(It.IsAny<string>())).ReturnsAsync(new Core.Models.OrganisationViewModel
+        {
+            ServiceName = "Service Name",
+            MinAge = 2,
+            MaxAge = 15,
+            Languages = new List<string>() { "English", "French" },
+            IsPayedFor = "Yes",
+            PayUnit = "Hour",
+            Cost = 2.50M,
+            CostDetails = "Some Cost Details",
+            HasSetDaysAndTimes = true,
+            DaySelection = new List<string> { "Monday", "Tuesday" },
+            IsSameTimeOnEachDay = true,
+            ServiceDeliverySelection = new List<string> { "1" },
+            Address1 = "First Line",
+            City = "City",
+            StateProvince = "StateProvince",
+            PostalCode = "PostCode",
+            InPersonSelection = new List<string> { "1" }
+        });
+        int callbask = 0;
+        _mockRequestDistributedCache.Setup(x => x.SetPageAsync(It.IsAny<string>(), It.IsAny<string>()))
+            .Callback(() => callbask++);
+
+        // Act
+        await _inPersonWhere.OnGet();
+
+        // Assert
+        callbask.Should().Be(1);
+        _inPersonWhere.Address1.Should().Be("First Line");
+        _inPersonWhere.Address2.Should().BeNull();
+        _inPersonWhere.City.Should().Be("City");
+        _inPersonWhere.StateProvince.Should().Be("StateProvince");
+        _inPersonWhere.PostalCode.Should().Be("PostCode");
+
+    }
+
+    [Fact]
     public async Task ThenServiceDeliveryTypeOnGet_WithNoViewModel()
     {
         //Arrange
@@ -123,6 +158,14 @@ public class InPersonWhereTests
     public async Task ThenServiceDeliveryTypeOnPost_ReturnNextPage()
     {
         //Arrange
+        var response = new PostcodesIoResponse();
+        response.Result = new PostcodeInfo
+        {
+            Longitude = 1.0,
+            Latitude = 2.0,
+        };
+
+        _mockPostcodeLocationClientService.Setup(x => x.LookupPostcode(It.IsAny<string>())).ReturnsAsync(response);
         _inPersonWhere.InPersonSelection = new List<string>();
         _inPersonWhere.Address1 = "Address1";
         _inPersonWhere.City = "City";
@@ -147,6 +190,14 @@ public class InPersonWhereTests
     public async Task ThenServiceDeliveryTypeOnPost_ReturnCheckServiceDetailsPage()
     {
         //Arrange
+        var response = new PostcodesIoResponse();
+        response.Result = new PostcodeInfo
+        {
+            Longitude = 1.0,
+            Latitude = 2.0,
+        };
+
+        _mockPostcodeLocationClientService.Setup(x => x.LookupPostcode(It.IsAny<string>())).ReturnsAsync(response);
         _inPersonWhere.InPersonSelection = new List<string>();
         _inPersonWhere.Address1 = "Address1";
         _inPersonWhere.City = "City";
@@ -167,4 +218,20 @@ public class InPersonWhereTests
         result.PageName.Should().Be("CheckServiceDetails");
     }
 
+    [Fact]
+    public async Task ThenServiceDeliveryTypeOnPost_ReturnInValidPostCode()
+    {
+        // Arrange
+        _mockPostcodeLocationClientService.Setup(x => x.LookupPostcode(It.IsAny<string>())).Throws<Exception>();
+        _inPersonWhere.InPersonSelection = new List<string>();
+        _inPersonWhere.Address1 = "Address1";
+        _inPersonWhere.City = "City";
+        _inPersonWhere.PostalCode = "Some Post Code";
+
+        // Act
+        await _inPersonWhere.OnPost();
+
+        // Assert
+        _inPersonWhere.PostcodeApiValid.Should().BeFalse();
+    }
 }
