8b4d5a057371a12977dc3ecb0feaec34a423e273
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-08-04 11:41:29 +0100

    Added TimesEachDayTests

--- Test/FamilyHubs.ServiceDirectory.Admin.Web.UnitTests/Areas/ServiceWizzard/WhenServiceTakesPlaceTests.cs
+++ Test/FamilyHubs.ServiceDirectory.Admin.Web.UnitTests/Areas/ServiceWizzard/WhenServiceTakesPlaceTests.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
