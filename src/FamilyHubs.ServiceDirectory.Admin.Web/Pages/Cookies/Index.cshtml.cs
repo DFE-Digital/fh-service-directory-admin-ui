ad90795495b0ce8fd73957c65e8fa4bd6e4ffa25
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2023-09-06 11:54:13 +0100

    FHG-4291 Agree to Terms and Conditions (#239)
    Co-authored-by: Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Cookies/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using FamilyHubs.SharedKernel.Razor.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.Cookies
+{
+    public class IndexModel : PageModel
+    {
+
+    }
+}
