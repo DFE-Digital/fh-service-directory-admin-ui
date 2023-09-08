2b1f6ecf4dac28a244072dbd7fa3554005de6ba9
Phil Davies <github@phildavies.co.uk>
2023-09-08 14:23:05 +0100

    use config to populate contact us page

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Contact-Us/Index.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Contact-Us/Index.cshtml.cs
@@ -1,11 +1,19 @@
 using FamilyHubs.ServiceDirectory.Admin.Web.Extensions;
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.Contact_Us;
 
 public class IndexModel : PageModel
 {
     public string PreviousPageLink { get; set; } = string.Empty;
+    public IFamilyHubsUiOptions FamilyHubsUiOptions { get; set; }
+
+    public IndexModel(IOptions<FamilyHubsUiOptions> familyHubsUiOptions)
+    {
+        FamilyHubsUiOptions = familyHubsUiOptions.Value;
+    }
 
     public void OnGet()
     {
