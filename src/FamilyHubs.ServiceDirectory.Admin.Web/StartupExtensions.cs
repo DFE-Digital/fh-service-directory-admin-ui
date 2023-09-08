c1d6b2a7f1db22a9a92b1c13aa30b9831a6d3959
Phil Davies <github@phildavies.co.uk>
2023-09-08 14:46:39 +0100

    error handling is handled by fh packages

--- src/FamilyHubs.ServiceDirectory.Admin.Web/StartupExtensions.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/StartupExtensions.cs
@@ -247,11 +247,9 @@ public static class StartupExtensions
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
-            app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
-        app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 #if use_https
         app.UseHttpsRedirection();
