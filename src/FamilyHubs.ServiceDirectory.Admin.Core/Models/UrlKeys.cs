09e40db3ccf7b9fd65096242101a0c9db1e11d83
Witek <witek.poreda@education.gov.uk>
2023-10-11 13:46:52 +0100

    new index page content with links to other apps

--- src/FamilyHubs.ServiceDirectory.Admin.Core/Models/UrlKeys.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Core/Models/UrlKeys.cs
@@ -3,6 +3,7 @@
     public enum UrlKeys
     {
         ConnectWeb,
-        ManageWeb
+        ManageWeb, 
+        FindWeb
     }
 }
--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/index.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/index.cshtml.cs
@@ -1,17 +1,32 @@
-﻿using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
+﻿using FamilyHubs.ServiceDirectory.Admin.Core.Models;
+using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 using FamilyHubs.SharedKernel.Identity;
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages;
 
 public class IndexModel : HeaderPageModel
 {
+    private readonly FamilyHubsUiOptions _familyHubsUiOptions;
+
+    public string ConnectUrl { get; set; } = string.Empty;
+    public string FindUrl { get; set; } = string.Empty;
+
+    public IndexModel(IOptions<FamilyHubsUiOptions> configuration)
+    {
+        _familyHubsUiOptions = configuration.Value;
+    }
     public IActionResult OnGet()
     {
         if (HttpContext.IsUserLoggedIn())
         {
             return RedirectToPage("/Welcome");
         }
+
+        ConnectUrl = _familyHubsUiOptions.Url(UrlKeys.ConnectWeb).ToString();
+        FindUrl = _familyHubsUiOptions.Url(UrlKeys.FindWeb).ToString();
 
         return Page();
     }
