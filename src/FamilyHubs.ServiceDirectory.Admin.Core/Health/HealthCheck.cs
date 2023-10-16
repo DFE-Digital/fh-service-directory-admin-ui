eb78de2e52c35f7c858f037fdf81f9ae74ce29e3
Phil Davies <github@phildavies.co.uk>
2023-10-16 12:16:53 +0100

    add health check wip

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Core/Health/HealthCheck.cs
@@ -0,0 +1,99 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Core.Health;
+
+public static class HealthCheck
+{
+    public enum UrlType
+    {
+        InternalApi,
+        ExternalApi,
+        InternalSite,
+        ExternalSite
+    }
+
+    public static IHealthChecksBuilder AddApi(
+        this IHealthChecksBuilder builder,
+        string name,
+        string configKey,
+        IConfiguration configuration,
+        UrlType urlType = UrlType.InternalApi)
+    {
+        string? apiUrl = configuration.GetValue<string>(configKey);
+
+        // Only add the health check if the config key is set.
+        // Either the API is optional (or not used locally) and missing intentionally,
+        // in which case there's no need to add the health check,
+        // or it's required, but in that case, the real consumer of the API should
+        // continue to throw it's own relevant exception
+        if (!string.IsNullOrEmpty(apiUrl))
+        {
+            if (urlType == UrlType.InternalApi)
+            {
+                //todo: add "/health" endpoints to all APIs
+
+                apiUrl = apiUrl.TrimEnd('/') + "/api/info";
+            }
+
+            // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
+            builder.AddUrlGroup(new Uri(apiUrl), name, HealthStatus.Degraded,
+                new[] { urlType.ToString() });
+        }
+
+        return builder;
+    }
+
+    public static IServiceCollection AddSiteHealthChecks(
+        this IServiceCollection services,
+        IConfiguration config)
+    {
+        var oneLoginUrl = config.GetValue<string>("GovUkOidcConfiguration:Oidc:BaseUrl");
+        var sqlServerCacheConnectionString = config.GetValue<string>("CacheConnection");
+
+        // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
+        var healthCheckBuilder = services.AddHealthChecks()
+            .AddIdentityServer(new Uri(oneLoginUrl!), name: "One Login", failureStatus: HealthStatus.Degraded, tags: new[] { "ExternalAPI" })
+            //todo: family hubs helper? pick up from config?
+            .AddApi("Feedback Site", "FamilyHubsUi:FeedbackUrl", config, UrlType.ExternalSite)
+            .AddApi("Service Directory API", "ServiceDirectoryApiBaseUrl", config)
+            .AddApi("Referral API", "ReferralApiBaseUrl", config)
+            .AddApi("Idams API", "GovUkOidcConfiguration:IdamsApiBaseUrl", config)
+            .AddSqlServer(sqlServerCacheConnectionString!, failureStatus: HealthStatus.Degraded, tags: new[] { "Database" });
+
+        string? notificationApiUrl = config.GetValue<string>("Notification:Endpoint");
+        if (!string.IsNullOrEmpty(notificationApiUrl))
+        {
+            // special case as Url contains path
+            //todo: change notifications client to use host and append path
+            notificationApiUrl = notificationApiUrl.Replace("/api/notify", "/api/info");
+            healthCheckBuilder.AddUrlGroup(new Uri(notificationApiUrl), "Notification API", HealthStatus.Degraded,
+                new[] { UrlType.InternalApi.ToString() });
+        }
+
+        // not usually set running locally
+        string? aiInstrumentationKey = config.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY");
+        if (!string.IsNullOrEmpty(aiInstrumentationKey))
+        {
+            //todo: check in dev env
+            healthCheckBuilder.AddAzureApplicationInsights(aiInstrumentationKey, "App Insights", HealthStatus.Degraded, new[] { "Infrastructure" });
+        }
+
+        return services;
+    }
+
+    public static WebApplication MapSiteHealthChecks(this WebApplication app)
+    {
+        app.MapHealthChecks("/health", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+
+        return app;
+    }
+}
