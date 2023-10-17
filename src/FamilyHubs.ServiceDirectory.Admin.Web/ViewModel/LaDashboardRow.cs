eff282210a97f87aff51231f6330cb432188782d
MartinBelton-gov <111495215+MartinBelton-gov@users.noreply.github.com>
2023-10-17 15:23:14 +0100

    Remove Link for Subject Access Request (#274)
    Co-authored-by: Martin-Belton-gov <martin.belton@digital.education.gov.uk>

--- src/FamilyHubs.ServiceDirectory.Admin.Web/ViewModel/LaDashboardRow.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/ViewModel/LaDashboardRow.cs
@@ -6,12 +6,10 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
 
 public class LaDashboardRow : IRow<ReferralDto>
 {
-    private readonly Uri _laWebBaseUrl;
     public ReferralDto Item { get; }
 
     public LaDashboardRow(ReferralDto referral, Uri laWebBaseUrl)
     {
-        _laWebBaseUrl = laWebBaseUrl;
         Item = referral;
     }
 
@@ -19,8 +17,7 @@ public class LaDashboardRow : IRow<ReferralDto>
     {
         get
         {
-            var requestDetailsUrl = $"{_laWebBaseUrl}La/RequestDetails?id={Item.Id}";
-            yield return new Cell($"<a href=\"{requestDetailsUrl}\">{HttpUtility.HtmlEncode(Item.RecipientDto.Name)}</a>");
+            yield return new Cell(HttpUtility.HtmlEncode(Item.RecipientDto.Name));
             yield return new Cell(Item.ReferralServiceDto.Name);
             yield return new Cell(Item.LastModified?.ToString("dd MMM yyyy") ?? "");
             yield return new Cell(Item.Created?.ToString("dd MMM yyyy") ?? "");
