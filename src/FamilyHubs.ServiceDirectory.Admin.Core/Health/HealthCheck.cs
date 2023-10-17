f30bca70335a25f9835bb992debf51f38a780489
Phil Davies <github@phildavies.co.uk>
2023-10-17 15:25:05 +0100

    tidy up

--- src/FamilyHubs.ServiceDirectory.Admin.Core/Health/HealthCheck.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Core/Health/HealthCheck.cs
@@ -1,4 +1,5 @@
-﻿using HealthChecks.UI.Client;
+﻿using FamilyHubs.SharedKernel.Razor.Health;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Configuration;
@@ -9,45 +10,6 @@ namespace FamilyHubs.ServiceDirectory.Admin.Core.Health;
 
 public static class HealthCheck
 {
-    public enum UrlType
-    {
-        InternalApi,
-        ExternalApi,
-        InternalSite,
-        ExternalSite
-    }
-
-    public static IHealthChecksBuilder AddApi(
-        this IHealthChecksBuilder builder,
-        string name,
-        string configKey,
-        IConfiguration configuration,
-        UrlType urlType = UrlType.InternalApi)
-    {
-        string? apiUrl = configuration.GetValue<string>(configKey);
-
-        // Only add the health check if the config key is set.
-        // Either the API is optional (or not used locally) and missing intentionally,
-        // in which case there's no need to add the health check,
-        // or it's required, but in that case, the real consumer of the API should
-        // continue to throw it's own relevant exception
-        if (!string.IsNullOrEmpty(apiUrl))
-        {
-            if (urlType == UrlType.InternalApi)
-            {
-                //todo: add "/health" endpoints to all APIs
-
-                apiUrl = apiUrl.TrimEnd('/') + "/api/info";
-            }
-
-            // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
-            builder.AddUrlGroup(new Uri(apiUrl), name, HealthStatus.Degraded,
-                new[] { urlType.ToString() });
-        }
-
-        return builder;
-    }
-
     public static IServiceCollection AddSiteHealthChecks(
         this IServiceCollection services,
         IConfiguration config)
@@ -66,20 +28,9 @@ public static class HealthCheck
             //todo: change notifications client to use host and append path
             notificationApiUrl = notificationApiUrl.Replace("/api/notify", "/api/info");
             healthCheckBuilder.AddUrlGroup(new Uri(notificationApiUrl), "Notification API", HealthStatus.Degraded,
-                new[] { UrlType.InternalApi.ToString() });
+                new[] { FhHealthChecksBuilder.UrlType.InternalApi.ToString() });
         }
 
         return services;
-    }
-
-    public static WebApplication MapSiteHealthChecks(this WebApplication app)
-    {
-        app.MapHealthChecks("/health", new HealthCheckOptions
-        {
-            Predicate = _ => true,
-            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-        });
-
-        return app;
     }
 }
