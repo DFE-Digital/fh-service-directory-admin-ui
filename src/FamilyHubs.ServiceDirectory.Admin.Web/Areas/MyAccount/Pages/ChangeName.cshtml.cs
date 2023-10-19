0123159e8920788277cee5605b5fb3569faab955
Phil Davies <github@phildavies.co.uk>
2023-10-19 12:24:12 +0100

    change name wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/MyAccount/Pages/ChangeName.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/MyAccount/Pages/ChangeName.cshtml.cs
@@ -1,14 +1,28 @@
+using System.Collections.Immutable;
 using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
 using FamilyHubs.SharedKernel.Identity;
+using FamilyHubs.SharedKernel.Razor.Errors;
 using Microsoft.AspNetCore.Mvc;
+using ErrorDictionary = System.Collections.Immutable.ImmutableDictionary<int, FamilyHubs.SharedKernel.Razor.Errors.Error>;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.MyAccount.Pages;
 
 public class ChangeNameModel : MyAccountViewModel
 {
+    public enum ErrorId
+    {
+        EnterAName
+    }
+
+    public static readonly ErrorDictionary AllErrors = ImmutableDictionary
+        .Create<int, Error>()
+        .Add(ErrorId.EnterAName, "consent-yes", "Enter a name");
+
     private readonly IIdamClient _idamClient;
+
+    public IErrorState ErrorState { get; private set; }
 
     [BindProperty]
     public string FullName { get; set; } = string.Empty;        
@@ -17,9 +31,11 @@ public class ChangeNameModel : MyAccountViewModel
     {
         PreviousPageLink = "/ViewPersonalDetails";
         ErrorMessage = "Enter a name";
-        PageHeading = "Change your name";
+        //PageHeading = "Change your name";
         HasBackButton = true;
         _idamClient = idamClient;
+
+        ErrorState = SharedKernel.Razor.Errors.ErrorState.Empty;
     }
 
     public void OnGet()
@@ -30,7 +46,6 @@ public class ChangeNameModel : MyAccountViewModel
 
     public async Task<IActionResult> OnPost()
     {
-            
         if (ModelState.IsValid && !string.IsNullOrWhiteSpace(FullName) && FullName.Length <= 255)
         {
             var familyHubsUser = HttpContext.GetFamilyHubsUser();
@@ -47,7 +62,9 @@ public class ChangeNameModel : MyAccountViewModel
             return RedirectToPage("ChangeNameConfirmation");
         }
 
-        HasValidationError = true;
+        //todo: overload/replace with params version
+        ErrorState = SharedKernel.Razor.Errors.ErrorState.Create(AllErrors, new[] {ErrorId.EnterAName});
+        //HasValidationError = true;
 
         return Page();
     }
