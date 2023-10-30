9bda49379ca57eef992bc2a78371fdaf934cbadd
witpo <witek.poreda@digital.education.gov.uk>
2023-10-30 13:36:54 +0000

    #FHG-4545 Add updated accessibility statement (#286)
    * #FHG-4545 Add updated accessibility statement
    
    * use new-tab shared component
    
    * all links updated to use new-tab
    
    ---------
    
    Co-authored-by: Witek <witek.poreda@education.gov.uk>

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Accessibility.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Accessibility.cshtml.cs
@@ -1,14 +1,23 @@
 using FamilyHubs.ServiceDirectory.Admin.Web.Extensions;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
+using Microsoft.Extensions.Options;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages;
 
 public class AccessibilityModel : HeaderPageModel
 {
-    public string PreviousPageLink { get; set; } = string.Empty;
+    private readonly FamilyHubsUiOptions _configuration;
 
+    public string PreviousPageLink { get; set; } = string.Empty;
+    public string SupportEmail { get; set; } = string.Empty;
+    public AccessibilityModel(IOptions<FamilyHubsUiOptions> configuration)
+    {
+        _configuration = configuration.Value;
+    }
     public void OnGet()
     {
         PreviousPageLink = HttpContext.GetBackButtonPath();
+        SupportEmail = _configuration.SupportEmail;
     }
 }
