2cc2648255572a88ba61ea4abb74dd882d9f561e
Phil Davies <github@phildavies.co.uk>
2023-10-30 14:59:51 +0000

    back button wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Terms-and-conditions/Index.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Terms-and-conditions/Index.cshtml.cs
@@ -1,5 +1,4 @@
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
-using FamilyHubs.ServiceDirectory.Admin.Web.Extensions;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
 using Microsoft.Extensions.Options;
@@ -8,22 +7,13 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.Terms_and_conditions;
 
 public class IndexModel : HeaderPageModel
 {
-    private readonly FamilyHubsUiOptions _configuration;
-
-    public string PreviousPageLink { get; set; } = string.Empty;
-    public string ConnectPrivacyNoticeUrl { get; set; } = string.Empty;
-
-    public string SupportEmail { get; set;} = string.Empty;
+    public Uri ConnectPrivacyNoticeUrl { get; set; }
+    public string SupportEmail { get; set;}
 
     public IndexModel(IOptions<FamilyHubsUiOptions> configuration)
     {
-        _configuration = configuration.Value;
-    }
-
-    public void OnGet()
-    {
-        PreviousPageLink = HttpContext.GetBackButtonPath();
-        ConnectPrivacyNoticeUrl = _configuration.Url(UrlKeys.ConnectWeb, "/privacy").ToString();
-        SupportEmail = _configuration.SupportEmail;
+        var config = configuration.Value;
+        ConnectPrivacyNoticeUrl = config.Url(UrlKeys.ConnectWeb, "/privacy");
+        SupportEmail = config.SupportEmail;
     }
 }
