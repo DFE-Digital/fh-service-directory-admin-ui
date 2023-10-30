04ad0506e5959ebdb74d29400b9351a352636a7a
Witek <witek.poreda@education.gov.uk>
2023-10-30 10:24:55 +0000

    FHG-4538 Update terms and conditions

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Terms-and-conditions/Index.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Terms-and-conditions/Index.cshtml.cs
@@ -1,14 +1,26 @@
+using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Extensions;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
+using Microsoft.Extensions.Options;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.Terms_and_conditions;
 
 public class IndexModel : HeaderPageModel
 {
+    private readonly FamilyHubsUiOptions _configuration;
+
     public string PreviousPageLink { get; set; } = string.Empty;
+    public string ConnectPrivacyNoticeUrl { get; set; } = string.Empty;
+
+    public IndexModel(IOptions<FamilyHubsUiOptions> configuration)
+    {
+        _configuration = configuration.Value;
+    }
 
     public void OnGet()
     {
         PreviousPageLink = HttpContext.GetBackButtonPath();
+        ConnectPrivacyNoticeUrl = _configuration.Url(UrlKeys.ConnectWeb, "/privacy").ToString();
     }
 }
