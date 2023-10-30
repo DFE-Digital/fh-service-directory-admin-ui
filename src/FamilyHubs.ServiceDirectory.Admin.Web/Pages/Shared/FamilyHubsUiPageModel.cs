13e13b2226ab0cffb541d4e23f8f99d59659277f
Phil Davies <github@phildavies.co.uk>
2023-10-30 14:52:38 +0000

    fix back button wip + use contact us component

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Shared/FamilyHubsUiPageModel.cs
@@ -0,0 +1,15 @@
+﻿using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
+using Microsoft.Extensions.Options;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
+
+//todo: move to web components
+public class FamilyHubsUiPageModel : HeaderPageModel
+{
+    public IFamilyHubsUiOptions FamilyHubsUiOptions { get; }
+
+    public FamilyHubsUiPageModel(IOptions<FamilyHubsUiOptions> familyHubsUiOptions)
+    {
+        FamilyHubsUiOptions = familyHubsUiOptions.Value;
+    }
+}
