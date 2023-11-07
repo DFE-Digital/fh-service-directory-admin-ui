4c6e8cd798edda3892b9d355f22132050e67251d
Witek <witek.poreda@education.gov.uk>
2023-11-07 14:34:12 +0000

    prep UI for multiple user types wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/Locations/Pages/ManageLocations.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/Locations/Pages/ManageLocations.cshtml.cs
@@ -1,4 +1,5 @@
 using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
+using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Shared.Dto;
 using FamilyHubs.ServiceDirectory.Shared.Enums;
 using FamilyHubs.SharedKernel.Identity;
@@ -22,13 +23,13 @@ public class LocationDashboardRow : IRow<LocationDto>
     {
         get
         {
-            yield return new Cell(GetLocationDescryption(Item));
+            yield return new Cell(GetLocationDescription(Item));
             yield return new Cell(GetLocationType(Item));
             yield return new Cell($"<a href=\"/locations/LocationDetail?id={Item.Id}\">View</a>");
         }
     }
 
-    private string GetLocationDescryption(LocationDto location)
+    private string GetLocationDescription(LocationDto location)
     {
         var parts = new string[] { Item.Name, Item.Address1, Item.Address2 ?? "", Item.City, Item.PostCode };
         return string.Join(", ", parts.Where(p=> !string.IsNullOrEmpty(p)));
@@ -44,6 +45,7 @@ public class LocationDashboardRow : IRow<LocationDto>
 public class ManageLocationsModel : PageModel, IDashboard<LocationDto>
 {
     public string? Title { get; set; }
+    public string? SubTitle { get; set; }
 
     private enum Column
     {
@@ -77,6 +79,7 @@ public class ManageLocationsModel : PageModel, IDashboard<LocationDto>
 
     public async Task OnGet(string? columnName, SortOrder sort, int? currentPage = 1)
     {
+        var user = HttpContext.GetFamilyHubsUser();
         if (columnName == null || !Enum.TryParse(columnName, true, out Column column))
         {
             // default when first load the page, or user has manually changed the url
@@ -85,21 +88,56 @@ public class ManageLocationsModel : PageModel, IDashboard<LocationDto>
         }
 
         _columnHeaders = new ColumnHeaderFactory(_columnImmutables, "/Locations/ManageLocations", column.ToString(), sort).CreateAll();
-        var locations = await _serviceDirectoryClient.GetLocations(sort == SortOrder.ascending, column.ToString(), currentPage!.Value);
+
+        var locations = new Shared.Models.PaginatedList<LocationDto>();
+        if ( user.Role == RoleTypes.DfeAdmin )
+        {
+             locations = await _serviceDirectoryClient.GetLocations(sort == SortOrder.ascending, column.ToString(), currentPage!.Value);
+        }
+        else
+        {
+            long organisationId;
+            long.TryParse(user.OrganisationId, out organisationId);
+            locations = await _serviceDirectoryClient.GetLocationsByOrganisationId(organisationId, sort == SortOrder.ascending, column.ToString(), currentPage!.Value);
+        }
+        
         _rows = locations.Items.Select(r => new LocationDashboardRow(r));        
 
         Pagination = new LargeSetLinkPagination<Column>("/Locations/ManageLocations", locations.TotalPages, currentPage!.Value, column, sort);        
 
-        var user = HttpContext.GetFamilyHubsUser();
+        
+        var organisationName = await GetOrganisationName(user.OrganisationId);
         Title = user.Role switch
         {
-            RoleTypes.DfeAdmin => "Services",
-            RoleTypes.LaManager or RoleTypes.LaDualRole => "[Local authority] services",
-            RoleTypes.VcsManager or RoleTypes.VcsDualRole => "[VCS organisation] services",
+            RoleTypes.DfeAdmin => "Locations",
+            RoleTypes.LaManager or RoleTypes.LaDualRole => $"{organisationName} Locations",
+            RoleTypes.VcsManager or RoleTypes.VcsDualRole => $"{organisationName} Locations",
+            _ => throw new InvalidOperationException($"Unknown role: {user.Role}")
+        };
+        SubTitle = user.Role switch
+        {
+            RoleTypes.DfeAdmin => "View existing locations",
+            RoleTypes.LaManager or RoleTypes.LaDualRole => "View existing locations in your local authority and add locations",
+            RoleTypes.VcsManager or RoleTypes.VcsDualRole => "View existing locations in your organisation",
             _ => throw new InvalidOperationException($"Unknown role: {user.Role}")
         };
     }
+    
+    private async Task<string> GetOrganisationName(string organisationIdString)
+    {
+        long organisationId;
+        if (long.TryParse(organisationIdString, out organisationId))
+        {
+            var organisation = await _serviceDirectoryClient.GetOrganisationById(organisationId);
+            if (organisation is not null)
+            {
+                return organisation.Name;
+            }
+        }
+        
+        return "";
+    }
 
-  
-   
+
+
 }
