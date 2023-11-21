f985bc49b789cbf266d4be47350ea4c19b3e2db6
Phil Davies <github@phildavies.co.uk>
2023-11-21 12:38:55 +0000

    update packages and service name page wip

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Service-Name/Index.cshtml.cs
@@ -0,0 +1,44 @@
+using System.Collections.Immutable;
+using FamilyHubs.SharedKernel.Razor.Errors;
+using FamilyHubs.SharedKernel.Razor.FullPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.Service_Name;
+
+/// <summary>
+/// Simple example, not production ready, no P/R/G, loading existing value, retaining value etc.
+/// </summary>
+public class IndexModel : PageModel, ISingleTextboxPageModel
+{
+    public string? HeadingText { get; set; }
+    public string? HintText { get; set; }
+    public string TextBoxLabel { get; set; } = "What is the service name?";
+    public IErrorState Errors { get; set; } = ErrorState.Empty;
+
+    [BindProperty]
+    public string? TextBoxValue { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public void OnPost()
+    {
+        if (string.IsNullOrWhiteSpace(TextBoxValue))
+        {
+            Errors = ErrorState.Create(PossibleErrors, new[] { ErrorId.AnswerMissing });
+        }
+    }
+
+    public enum ErrorId
+    {
+        AnswerMissing,
+        AnswerTooLong
+    }
+
+    public static readonly ImmutableDictionary<int, SharedKernel.Razor.Errors.Error> PossibleErrors =
+        ImmutableDictionary.Create<int, SharedKernel.Razor.Errors.Error>()
+            .Add(ErrorId.AnswerMissing, ISingleTextboxPageModel.TextBoxId, "Guru meditation required")
+            .Add(ErrorId.AnswerTooLong, ISingleTextboxPageModel.TextBoxId, "The answer is too long");
+}
