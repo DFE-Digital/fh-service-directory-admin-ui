7668264197fe4c946ca792f82e4530b6be52b934
Witek <witek.poreda@education.gov.uk>
2023-11-21 14:54:59 +0000

    some data displayed

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/ServiceDetail.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/ServiceDetail.cshtml.cs
@@ -1,6 +1,9 @@
 using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
+using FamilyHubs.ServiceDirectory.Shared.Dto;
 using Microsoft.AspNetCore.Authorization;
+using System.Globalization;
+using System.Runtime;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
 
@@ -9,6 +12,21 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
 public class ServiceDetailModel : HeaderPageModel
 {
     public string? Title { get; set; }
+    public List<TaxonomyDto> Taxonomies { get; set; } = new();
+
+    public string Description { get; set; } = "";
+
+
+    public string Languages { get; set; } = "";
+
+    public List<string> Costs { get; set; } = new();
+
+    public List<ContactDto> Contacts { get; set; } = new List<ContactDto>();
+
+    public string? Eligibility { get; set; } = "";
+
+    public List<string> Shedule { get; set; } = new();
+
 
     private readonly IServiceDirectoryClient _serviceDirectoryClient;
 
@@ -17,8 +35,84 @@ public class ServiceDetailModel : HeaderPageModel
         _serviceDirectoryClient = serviceDirectoryClient;
     }
 
-    public void OnGet()
+    public async Task OnGetAsync(long id)
     {
+        var service = await _serviceDirectoryClient.GetServiceById(id);
 
+        if (service != null)
+        {
+            Title = service.Name;
+            Taxonomies = service.Taxonomies.ToList();
+            Description = service.Description ?? "";
+            Languages = string.Join(", ", service.Languages.Select(x => x.Name));
+            Contacts = service.Contacts.ToList();
+            Costs = GetCost(service).ToList();
+            Eligibility = GetAgeRange(service.Eligibilities.FirstOrDefault());
+            Shedule = GetWhen(service).ToList();
+        }
+
+    }
+
+    private static IEnumerable<string> GetWhen(ServiceDto service)
+    {
+        var when =
+            service.RegularSchedules.FirstOrDefault()?.Description?.Split('\n').Select(l => l.Trim())
+            ?? Enumerable.Empty<string>();
+        return when;
+    }
+
+    private static string? GetAgeRange(EligibilityDto? eligibility)
+    {
+        var forChildAndYoungPeople = "No - ";
+        if (eligibility?.EligibilityType == ServiceDirectory.Shared.Enums.EligibilityType.Child
+            || eligibility?.EligibilityType == ServiceDirectory.Shared.Enums.EligibilityType.Teen)
+        {
+            forChildAndYoungPeople = "Yes - ";
+        }
+
+        return eligibility == null ? null : $"{forChildAndYoungPeople}{AgeToString(eligibility.MinimumAge)} to {AgeToString(eligibility.MaximumAge)}";
+    }
+
+    private static string AgeToString(int age)
+    {
+        return age == 127 ? "25+ years" : (age == 1 ? age.ToString() + " year" : age.ToString() + " years");
+    }
+
+    private static IEnumerable<string> GetCost(ServiceDto service)
+    {
+        const string Free = "Free";
+
+        if (!service.CostOptions.Any())
+        {
+            return new[] { Free };
+        }
+
+        var cost = new List<string>();
+        var firstCost = service.CostOptions.First();
+
+        if (firstCost.Amount is null or decimal.Zero)
+        {
+            cost.Add(Free);
+        }
+
+        if (firstCost.Amount is not null && firstCost.Amount != decimal.Zero)
+        {
+            var ukNumberFormat = new CultureInfo("en-GB", false).NumberFormat;
+            var amount = firstCost.Amount.Value.ToString("C0", ukNumberFormat);
+            var message = $"Yes - {amount}";
+            if (!string.IsNullOrWhiteSpace(firstCost.Option))
+            {
+                message += $" every {firstCost.Option?.ToLowerInvariant()}";
+            }
+
+            cost.Add(message);
+        }
+
+        if (!string.IsNullOrWhiteSpace(firstCost.AmountDescription))
+        {
+            cost.Add(firstCost.AmountDescription);
+        }
+
+        return cost;
     }
 }
