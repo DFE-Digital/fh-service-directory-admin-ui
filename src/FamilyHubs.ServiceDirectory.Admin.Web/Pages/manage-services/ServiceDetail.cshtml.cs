dda5520946da971e8e8f3736ea7f388c63f3d7f5
Phil Davies <github@phildavies.co.uk>
2023-11-27 13:32:23 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/ServiceDetail.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/ServiceDetail.cshtml.cs
@@ -1,12 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
+using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
+
+public class ServiceDetailModel : PageModel
 {
-    public class ServiceDetailModel : PageModel
+    public long ServiceId { get; set; }
+    public string? Title { get; set; }
+
+    private readonly IServiceDirectoryClient _serviceDirectoryClient;
+
+    public ServiceDetailModel(IServiceDirectoryClient serviceDirectoryClient)
     {
-        public void OnGet()
-        {
-        }
+        _serviceDirectoryClient = serviceDirectoryClient;
+    }
+
+    public async Task OnGetAsync(long serviceId)
+    {
+        ServiceId = serviceId;
+        var service = await _serviceDirectoryClient.GetServiceById(serviceId);
+        Title = service?.Name;
     }
 }
