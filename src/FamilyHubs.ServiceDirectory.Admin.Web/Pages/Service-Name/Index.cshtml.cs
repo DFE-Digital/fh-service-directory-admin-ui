997c5d121444e004fa725304454fb9be6b392b8e
Phil Davies <github@phildavies.co.uk>
2023-11-27 09:25:36 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Service-Name/Index.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Service-Name/Index.cshtml.cs
@@ -1,10 +1,8 @@
 using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
 using FamilyHubs.ServiceDirectory.Admin.Core.DistributedCache;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
-using FamilyHubs.ServiceDirectory.Admin.Web.Errors;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 using FamilyHubs.SharedKernel.Identity;
-using FamilyHubs.SharedKernel.Razor.ErrorNext;
 using FamilyHubs.SharedKernel.Razor.FullPages;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
