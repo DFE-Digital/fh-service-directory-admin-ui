f94d723437524955096a46db50c39d431e5594c6
Phil Davies <github@phildavies.co.uk>
2023-11-28 10:53:18 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/who-for.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/who-for.cshtml.cs
@@ -1,3 +1,4 @@
+using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
 using FamilyHubs.ServiceDirectory.Admin.Core.DistributedCache;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
@@ -7,9 +8,28 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services
 {
     public class who_forModel : ServiceWithCachePageModel
     {
-        public who_forModel(IRequestDistributedCache connectionRequestCache)
+        private readonly IServiceDirectoryClient _serviceDirectoryClient;
+
+        public who_forModel(
+            IRequestDistributedCache connectionRequestCache,
+            IServiceDirectoryClient serviceDirectoryClient)
             : base(ServiceJourneyPage.Who_For, connectionRequestCache)
         {
+            _serviceDirectoryClient = serviceDirectoryClient;
+        }
+
+        protected override async Task OnGetWithModelAsync(CancellationToken cancellationToken)
+        {
+            switch (Flow)
+            {
+                case JourneyFlow.Edit:
+                    var service = await _serviceDirectoryClient.GetServiceById(ServiceId!.Value, cancellationToken);
+                    break;
+
+                default:
+                    // pick up values from the cache
+                    break;
+            }
         }
 
         protected override Task<IActionResult> OnPostWithModelAsync(CancellationToken cancellationToken)
