b5011402ebb293a8f6b7f9aa9b8a61805df7e40a
Phil Davies <github@phildavies.co.uk>
2023-11-28 10:37:19 +0000

    journey harness wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/Support-Offered.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/Support-Offered.cshtml.cs
@@ -3,6 +3,7 @@ using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 using FamilyHubs.SharedKernel.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
 
@@ -12,5 +13,10 @@ public class Support_OfferedModel : ServiceWithCachePageModel
     public Support_OfferedModel(IRequestDistributedCache connectionRequestCache)
         : base(ServiceJourneyPage.Support_Offered, connectionRequestCache)
     {
+    }
+
+    protected override Task<IActionResult> OnPostWithModelAsync(CancellationToken cancellationToken)
+    {
+        return Task.FromResult(NextPage());
     }
 }
---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/who-for.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services
+{
+    public class who_forModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
