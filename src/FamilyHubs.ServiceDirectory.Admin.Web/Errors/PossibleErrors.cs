7be43f79ccde4a53df16dcd50eeb16cb10d9bdde
Phil Davies <github@phildavies.co.uk>
2023-12-01 11:05:19 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Errors/PossibleErrors.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Errors/PossibleErrors.cs
@@ -9,8 +9,8 @@ public static class PossibleErrors
     public static readonly ImmutableDictionary<int, PossibleError> All =
         ImmutableDictionary.Create<int, PossibleError>()
             .Add(ErrorId.Service_Name__EnterNameOfService, "Enter the name of the service")
-            .Add(ErrorId.Who_For__SelectYes, "Select yes if children or young people can use this service")
-            .Add(ErrorId.Who_For__SelectFromAge, "Select an age")
-            .Add(ErrorId.Who_For__SelectToAge, "Select an age")
+            .Add(ErrorId.Who_For__SelectYes, "Select whether children or young people can use this service")
+            .Add(ErrorId.Who_For__SelectFromAge, "Select age from")
+            .Add(ErrorId.Who_For__SelectToAge, "Select age to")
         ;
 }
