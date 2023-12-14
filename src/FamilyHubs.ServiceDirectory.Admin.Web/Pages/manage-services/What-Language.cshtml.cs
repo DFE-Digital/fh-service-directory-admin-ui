5354abe467eaab9924c6180610476b14a65d90c2
Phil Davies <github@phildavies.co.uk>
2023-12-14 14:45:43 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/What-Language.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/What-Language.cshtml.cs
@@ -8,15 +8,9 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
 
-//todo: store Code as well as language name? (The ISO 639-1 or ISO 639-3 code for the language.)
-//todo: where did the list of languages come from? do we need to support all ISO 639-1/3 languages? probably just use 639-1
-// prototype allows entering more languages that are in our list (e.g. in Find)
-//todo: if we have free text languages:
-// what happens when javascript is disabled? do they just have to spell it right
-// what happens when they enter a language that isn't in the list (e.g. misspelling Chinese Mandarin, rather than Chinese (Mandarin) )
-// do we accept all ISO languages, or just the ones in our list
-//todo: we'll have to change the existing languages in the db to use the new format (i.e. names match and add codes)
-//todo: add code to language (and use that in the Connect search)
+//todo: check javascript disabled
+//todo: adding another language duplicates the selected language
+//todo: update connect to use the code to search
 //todo: update Connect, so that the language names match
 
 public class WhatLanguageViewModel
@@ -34,6 +28,7 @@ public class What_LanguageModel : ServicePageModel<WhatLanguageViewModel>
     public const string AllLanguagesValue = "";
 
     // list taken from https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes#References
+    //todo: build up from LanguageDtoFactory or common list, so that there's one source of languages
     public static SelectListItem[] StaticLanguageOptions { get; set; } =
     {
         new() { Value = AllLanguagesValue, Text = "All languages", Selected = true, Disabled = true },
@@ -222,85 +217,6 @@ public class What_LanguageModel : ServicePageModel<WhatLanguageViewModel>
         new() { Value = "zu", Text = "Zulu" }
     };
 
-    //todo: in connect, use this list, but make names match and use code for value
-    //public static SelectListItem[] StaticLanguageOptions { get; set; } =
-    //{
-    //    new() { Value = AllLanguagesValue, Text = "All languages", Selected = true, Disabled = true },
-    //    new() { Value = "Afrikaans", Text = "Afrikaans" },
-    //    new() { Value = "Albanian", Text = "Albanian" },
-    //    new() { Value = "Arabic", Text = "Arabic" },
-    //    new() { Value = "Armenian", Text = "Armenian" },
-    //    new() { Value = "Basque", Text = "Basque" },
-    //    new() { Value = "Bengali", Text = "Bengali" },
-    //    new() { Value = "Bulgarian", Text = "Bulgarian" },
-    //    new() { Value = "Catalan", Text = "Catalan" },
-    //    new() { Value = "Cambodian", Text = "Cambodian" },
-    //    new() { Value = "Chinese (Mandarin)", Text = "Chinese (Mandarin)" },
-    //    new() { Value = "Croatian", Text = "Croatian" },
-    //    new() { Value = "Czech", Text = "Czech" },
-    //    new() { Value = "Danish", Text = "Danish" },
-    //    new() { Value = "Dutch", Text = "Dutch" },
-    //    new() { Value = "English", Text = "English"},
-    //    new() { Value = "Estonian", Text = "Estonian" },
-    //    new() { Value = "Fiji", Text = "Fiji" },
-    //    new() { Value = "Finnish", Text = "Finnish" },
-    //    new() { Value = "French", Text = "French" },
-    //    new() { Value = "Georgian", Text = "Georgian" },
-    //    new() { Value = "German", Text = "German" },
-    //    new() { Value = "Greek", Text = "Greek" },
-    //    new() { Value = "Gujarati", Text = "Gujarati" },
-    //    new() { Value = "Hebrew", Text = "Hebrew" },
-    //    new() { Value = "Hindi", Text = "Hindi" },
-    //    new() { Value = "Hungarian", Text = "Hungarian" },
-    //    new() { Value = "Icelandic", Text = "Icelandic" },
-    //    new() { Value = "Indonesian", Text = "Indonesian" },
-    //    new() { Value = "Irish", Text = "Irish" },
-    //    new() { Value = "Italian", Text = "Italian" },
-    //    new() { Value = "Japanese", Text = "Japanese" },
-    //    new() { Value = "Javanese", Text = "Javanese" },
-    //    new() { Value = "Korean", Text = "Korean" },
-    //    new() { Value = "Latin", Text = "Latin" },
-    //    new() { Value = "Latvian", Text = "Latvian" },
-    //    new() { Value = "Lithuanian", Text = "Lithuanian" },
-    //    new() { Value = "Macedonian", Text = "Macedonian" },
-    //    new() { Value = "Malay", Text = "Malay" },
-    //    new() { Value = "Malayalam", Text = "Malayalam" },
-    //    new() { Value = "Maltese", Text = "Maltese" },
-    //    new() { Value = "Maori", Text = "Maori" },
-    //    new() { Value = "Marathi", Text = "Marathi" },
-    //    new() { Value = "Mongolian", Text = "Mongolian" },
-    //    new() { Value = "Nepali", Text = "Nepali" },
-    //    new() { Value = "Norwegian", Text = "Norwegian" },
-    //    new() { Value = "Persian", Text = "Persian" },
-    //    new() { Value = "Polish", Text = "Polish" },
-    //    new() { Value = "Portuguese", Text = "Portuguese" },
-    //    new() { Value = "Punjabi", Text = "Punjabi" },
-    //    new() { Value = "Quechua", Text = "Quechua" },
-    //    new() { Value = "Romanian", Text = "Romanian" },
-    //    new() { Value = "Russian", Text = "Russian" },
-    //    new() { Value = "Samoan", Text = "Samoan" },
-    //    new() { Value = "Serbian", Text = "Serbian" },
-    //    new() { Value = "Slovak", Text = "Slovak" },
-    //    new() { Value = "Slovenian", Text = "Slovenian" },
-    //    new() { Value = "Somali", Text = "Somali" },
-    //    new() { Value = "Spanish", Text = "Spanish" },
-    //    new() { Value = "Swahili", Text = "Swahili" },
-    //    new() { Value = "Swedish ", Text = "Swedish " },
-    //    new() { Value = "Tamil", Text = "Tamil" },
-    //    new() { Value = "Tatar", Text = "Tatar" },
-    //    new() { Value = "Telugu", Text = "Telugu" },
-    //    new() { Value = "Thai", Text = "Thai" },
-    //    new() { Value = "Tibetan", Text = "Tibetan" },
-    //    new() { Value = "Tonga", Text = "Tonga" },
-    //    new() { Value = "Turkish", Text = "Turkish" },
-    //    new() { Value = "Ukrainian", Text = "Ukrainian" },
-    //    new() { Value = "Urdu", Text = "Urdu" },
-    //    new() { Value = "Uzbek", Text = "Uzbek" },
-    //    new() { Value = "Vietnamese", Text = "Vietnamese" },
-    //    new() { Value = "Welsh", Text = "Welsh" },
-    //    new() { Value = "Xhosa", Text = "Xhosa" },
-    //};
-
     public IEnumerable<SelectListItem> LanguageOptions => StaticLanguageOptions;
 
     public IEnumerable<string> LanguageCodes { get; set; }
@@ -326,12 +242,9 @@ public class What_LanguageModel : ServicePageModel<WhatLanguageViewModel>
         //todo: move error handling to method
         // base could call GetHandleErrors if HasErrors is true
 
-        //todo: need a state where there are no errors, but we've redirected to self, so we handle it similarly
-        //if (Errors.HasErrors)
         if (ServiceModel?.UserInput != null)
         {
             //todo: have viewmodel as property and bind - will it ignore languages?
-            //var viewModel = ServiceModel?.UserInput ?? throw new InvalidOperationException("ServiceModel.UserInput is null");
             var viewModel = ServiceModel.UserInput;
             LanguageCodes = viewModel.LanguageCodes;
             TranslationServices = viewModel.TranslationServices;
@@ -346,6 +259,7 @@ public class What_LanguageModel : ServicePageModel<WhatLanguageViewModel>
                     .Where(g => g.Count() > 1)
                     .Select(g => g.Skip(1).First().Index)
                     .FirstOrDefault();
+                
                 if (duplicateLanguageIndex != null)
                 {
                     ErrorToSelectIndex.Add((int)ErrorId.What_Language__SelectLanguageOnce, duplicateLanguageIndex.Value);
@@ -429,7 +343,7 @@ public class What_LanguageModel : ServicePageModel<WhatLanguageViewModel>
             var updatedLanguageCodes = languageCodes.Select(l => l ?? AllLanguagesValue).ToList();
             updatedLanguageCodes.Add(AllLanguagesValue);
             viewModel.LanguageCodes = updatedLanguageCodes;
-            //todo: will have to redirect to self with user input, but no errors!
+
             return RedirectToSelf(viewModel);
         }
         
