be2b3b4b1301680f18d6126e07d3c724bf9748a2
Phil Davies <github@phildavies.co.uk>
2023-12-21 14:17:21 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/What-Language.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/What-Language.cshtml.cs
@@ -32,8 +32,6 @@ public class What_LanguageModel : ServicePageModel<WhatLanguageViewModel>
 
     static What_LanguageModel()
     {
-        // list taken from https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes#References
-
         LanguageOptions = Languages.CodeToName
             .OrderBy(kv => kv.Value)
             .Select(kv => new SelectListItem(kv.Value, kv.Key))
