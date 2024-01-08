dcab86e7672eeb724700f72043fa7e6da2f352ff
Phil Davies <github@phildavies.co.uk>
2024-01-08 15:46:25 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/times.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/times.cshtml.cs
@@ -13,9 +13,9 @@ public enum AmPm
 
 //todo: separate for name/desc & values?
 //todo: doesn't belong with page. have in components?
-public class TimeModel
+public class TimeComponent
 {
-    public TimeModel(
+    public TimeComponent(
         string name,
         string? description = null,
         string? hintId = null,
@@ -33,34 +33,61 @@ public class TimeModel
     public string Name { get; set; }
     public AmPm DefaultAmPm { get; set; }
 
-    public int Hour { get; set; }
-    public int Minute { get; set; }
-    public AmPm AmPm { get; set; }
+    //public int Hour { get; set; }
+    //public int Minute { get; set; }
+    //public AmPm AmPm { get; set; }
+}
+
+public class TimeModel
+{
+    public int? Hour { get; set; }
+    public int? Minute { get; set; }
+    public AmPm? AmPm { get; set; }
+
+    public TimeModel(string name, IFormCollection form)
+    {
+        int value;
+        if (int.TryParse(form[$"{name}Hour"].ToString(), out value))
+        {
+            Hour = value;
+        }
+        if (int.TryParse(form[$"{name}Minute"].ToString(), out value))
+        {
+            Minute = value;
+        }
+        AmPm = form[$"{name}AmPm"].ToString() switch
+        {
+            "am" => manage_services.AmPm.Am,
+            "pm" => manage_services.AmPm.Pm,
+            _ => null
+        };
+    }
 }
 
 public class timesModel : ServicePageModel
 {
-    public TimeModel WeekdaysStarts { get; set; }
-    public TimeModel WeekdaysFinishes { get; set; }
-
-    public TimeModel WeekendsStarts { get; set; }
-    public TimeModel WeekendsFinishes { get; set; }
+    public static TimeComponent WeekdaysStarts => new("weekdaysStarts", "Starts", "weekdays-times-hint");
+    public static TimeComponent WeekdaysFinishes => new("weekdaysFinishes", "Finishes", "weekdays-times-hint", AmPm.Pm);
+    public static TimeComponent WeekendsStarts => new("weekendsStarts", "Starts", "weekends-times-hint");
+    public static TimeComponent WeekendsFinishes => new("weekendsFinishes", "Finishes", "weekends-times-hint", AmPm.Pm);
 
     public timesModel(IRequestDistributedCache connectionRequestCache)
         : base(ServiceJourneyPage.Times, connectionRequestCache)
     {
     }
 
-    protected override async Task OnGetWithModelAsync(CancellationToken cancellationToken)
+    protected override Task OnGetWithModelAsync(CancellationToken cancellationToken)
     {
-        WeekdaysStarts = new TimeModel("weekdays-starts", "Starts", "weekdays-times-hint", AmPm.Am);
-        WeekdaysFinishes = new TimeModel("weekdays-finishes", "Finishes", "weekdays-times-hint", AmPm.Pm);
-        WeekendsStarts = new TimeModel("weekends-starts", "Starts", "weekends-times-hint", AmPm.Am);
-        WeekendsFinishes = new TimeModel("weekends-finishes", "Finishes", "weekends-times-hint", AmPm.Pm);
+        return Task.CompletedTask;
     }
 
     protected override Task<IActionResult> OnPostWithModelAsync(CancellationToken cancellationToken)
     {
+        var weekdaysStarts = new TimeModel(WeekdaysStarts.Name, Request.Form);
+        var weekdaysFinishes = new TimeModel(WeekdaysFinishes.Name, Request.Form);
+        var weekendsStarts = new TimeModel(WeekendsStarts.Name, Request.Form);
+        var weekendsFinishes = new TimeModel(WeekendsFinishes.Name, Request.Form);
+
         return Task.FromResult(NextPage());
     }
 }
