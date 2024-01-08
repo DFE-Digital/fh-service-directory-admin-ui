c1d935e33659b820456e65ced0c4070d6db65061
Witek <witek.poreda@education.gov.uk>
2024-01-08 15:12:34 +0000

    add cost description to service details page

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/Service-Cost.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/Service-Cost.cshtml.cs
@@ -3,8 +3,6 @@ using FamilyHubs.ServiceDirectory.Admin.Core.DistributedCache;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 using FamilyHubs.ServiceDirectory.Shared.Dto;
-using FamilyHubs.SharedKernel.Identity;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
