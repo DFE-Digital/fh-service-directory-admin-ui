25164ae85cb765e12f23216a38477796a3459823
Phil Davies <github@phildavies.co.uk>
2024-01-08 13:21:17 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/times.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/times.cshtml.cs
@@ -1,13 +1,53 @@
+using Azure.Storage.Blobs.Models;
 using FamilyHubs.ServiceDirectory.Admin.Core.DistributedCache;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
 
+public enum AmPm
+{
+    Am,
+    Pm
+}
+
+//todo: separate for name/desc & values?
+//todo: doesn't belong with page. have in components?
+public class TimeModel
+{
+    public TimeModel(string name, string? description = null)
+    {
+        Name = name;
+        Description = description;
+        //Hour = 12;
+        //Minute = 0;
+        //AmPm = AmPm.Pm;
+    }
+
+    public string? Description { get; set; }
+    public string Name { get; set; }
+
+    public int Hour { get; set; }
+    public int Minute { get; set; }
+    public AmPm AmPm { get; set; }
+}
+
 public class timesModel : ServicePageModel
 {
+    public TimeModel WeekdaysStarts { get; set; }
+    public TimeModel WeekdaysFinishes { get; set; }
+
+    public TimeModel WeekendsStarts { get; set; }
+    public TimeModel WeekendsFinishes { get; set; }
+
     public timesModel(IRequestDistributedCache connectionRequestCache)
         : base(ServiceJourneyPage.Times, connectionRequestCache)
     {
+    }
+
+    protected override async Task OnGetWithModelAsync(CancellationToken cancellationToken)
+    {
+        WeekdaysStarts = new TimeModel("weekdays-starts", "Starts");
+        WeekdaysFinishes = new TimeModel("weekdays-finishes", "Finishes");
     }
 }
