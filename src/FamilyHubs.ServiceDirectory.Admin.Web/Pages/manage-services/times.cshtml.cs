d2b7cb00db42bfb4a3fc42cb0617c6d92d787ace
Phil Davies <github@phildavies.co.uk>
2024-01-09 11:35:51 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/times.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/times.cshtml.cs
@@ -1,9 +1,17 @@
+using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
 using FamilyHubs.ServiceDirectory.Admin.Core.DistributedCache;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
+using FamilyHubs.ServiceDirectory.Shared.Enums;
+using IdentityModel;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
+
+//todo: weekdays: FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR
+//weekends: FREQ=WEEKLY;BYDAY=SA,SU
+//both: 2 entries??
 
 public enum AmPm
 {
@@ -14,6 +22,12 @@ public enum AmPm
 //todo: doesn't belong with page. have in components?
 public class TimeComponent
 {
+    public string? Description { get; set; }
+    //todo: not nice having element id out of view
+    public string? HintId { get; set; }
+    public string Name { get; set; }
+    public AmPm DefaultAmPm { get; set; }
+
     public TimeComponent(
         string name,
         string? description = null,
@@ -26,11 +40,10 @@ public class TimeComponent
         DefaultAmPm = defaultAmPm;
     }
 
-    public string? Description { get; set; }
-    //todo: not nice having element id out of view
-    public string? HintId { get; set; }
-    public string Name { get; set; }
-    public AmPm DefaultAmPm { get; set; }
+    public TimeModel CreateModel(IFormCollection form)
+    {
+        return new TimeModel(Name, form);
+    }
 }
 
 public class TimeModel
@@ -39,10 +52,29 @@ public class TimeModel
     public int? Minute { get; set; }
     public AmPm? AmPm { get; set; }
 
+    public TimeModel(DateTime? time)
+    {
+        if (time == null)
+        {
+            return;
+        }
+
+        if (time.Value.Hour > 12)
+        {
+            Hour = time.Value.Hour - 12;
+            AmPm = manage_services.AmPm.Pm;
+        }
+        else
+        {
+            Hour = time.Value.Hour;
+            AmPm = manage_services.AmPm.Am;
+        }
+        Minute = time.Value.Minute;
+    }
+
     public TimeModel(string name, IFormCollection form)
     {
-        int value;
-        if (int.TryParse(form[$"{name}Hour"].ToString(), out value))
+        if (int.TryParse(form[$"{name}Hour"].ToString(), out var value))
         {
             Hour = value;
         }
@@ -61,27 +93,58 @@ public class TimeModel
 
 public class timesModel : ServicePageModel
 {
-    public static TimeComponent WeekdaysStarts => new("weekdaysStarts", "Starts", "weekdays-times-hint");
-    public static TimeComponent WeekdaysFinishes => new("weekdaysFinishes", "Finishes", "weekdays-times-hint", AmPm.Pm);
-    public static TimeComponent WeekendsStarts => new("weekendsStarts", "Starts", "weekends-times-hint");
-    public static TimeComponent WeekendsFinishes => new("weekendsFinishes", "Finishes", "weekends-times-hint", AmPm.Pm);
+    private readonly IServiceDirectoryClient _serviceDirectoryClient;
+    public static TimeComponent WeekdaysStartsComponent => new("weekdaysStarts", "Starts", "weekdays-times-hint");
+    public static TimeComponent WeekdaysFinishesComponent => new("weekdaysFinishes", "Finishes", "weekdays-times-hint", AmPm.Pm);
+    public static TimeComponent WeekendsStartsComponent => new("weekendsStarts", "Starts", "weekends-times-hint");
+    public static TimeComponent WeekendsFinishesComponent => new("weekendsFinishes", "Finishes", "weekends-times-hint", AmPm.Pm);
 
-    public timesModel(IRequestDistributedCache connectionRequestCache)
+    public TimeModel WeekdaysStarts { get; set; }
+    public TimeModel WeekdaysFinishes { get; set; }
+    public TimeModel WeekendsStarts { get; set; }
+    public TimeModel WeekendsFinishes { get; set; }
+
+    public timesModel(
+        IRequestDistributedCache connectionRequestCache,
+        IServiceDirectoryClient serviceDirectoryClient)
         : base(ServiceJourneyPage.Times, connectionRequestCache)
     {
+        _serviceDirectoryClient = serviceDirectoryClient;
     }
 
-    protected override Task OnGetWithModelAsync(CancellationToken cancellationToken)
+    protected override async Task OnGetWithModelAsync(CancellationToken cancellationToken)
     {
-        return Task.CompletedTask;
+        switch (Flow)
+        {
+            case JourneyFlow.Edit:
+                //todo: if edit flow, get service in base
+                var service = await _serviceDirectoryClient.GetServiceById(ServiceId!.Value, cancellationToken);
+                var weekday = service.RegularSchedules
+                    .FirstOrDefault(s => s is { Freq: FrequencyType.Weekly, ByDay: "MO,TU,WE,TH,FR" });
+                if (weekday != null)
+                {
+                    WeekdaysStarts = new TimeModel(weekday.OpensAt);
+                    WeekdaysFinishes = new TimeModel(weekday.ClosesAt);
+                }
+                var weekend = service.RegularSchedules
+                    .FirstOrDefault(s => s is { Freq: FrequencyType.Weekly, ByDay: "SA,SU" });
+                if (weekend != null)
+                {
+                    WeekendsStarts = new TimeModel(weekend.OpensAt);
+                    WeekendsFinishes = new TimeModel(weekend.ClosesAt);
+                }
+                break;
+            case JourneyFlow.Add:
+                break;
+        }
     }
 
     protected override Task<IActionResult> OnPostWithModelAsync(CancellationToken cancellationToken)
     {
-        var weekdaysStarts = new TimeModel(WeekdaysStarts.Name, Request.Form);
-        var weekdaysFinishes = new TimeModel(WeekdaysFinishes.Name, Request.Form);
-        var weekendsStarts = new TimeModel(WeekendsStarts.Name, Request.Form);
-        var weekendsFinishes = new TimeModel(WeekendsFinishes.Name, Request.Form);
+        WeekdaysStarts = WeekdaysStartsComponent.CreateModel(Request.Form);
+        WeekdaysFinishes = WeekdaysStartsComponent.CreateModel(Request.Form);
+        WeekendsStarts = WeekdaysStartsComponent.CreateModel(Request.Form);
+        WeekendsFinishes = WeekdaysStartsComponent.CreateModel(Request.Form);
 
         return Task.FromResult(NextPage());
     }
