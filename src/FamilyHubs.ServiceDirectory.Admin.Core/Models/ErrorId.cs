34f19648c00875911d5b417c9c60ce0a45145e1f
Phil Davies <github@phildavies.co.uk>
2024-01-09 15:08:24 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Core/Models/ErrorId.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Core/Models/ErrorId.cs
@@ -16,5 +16,17 @@ public enum ErrorId
     What_Language__SelectLanguageOnce,
     What_Language__EnterSupportedLanguage,
     Service_Cost__MissingSelection,
-    Service_Cost__DescriptionTooLong
+    Service_Cost__DescriptionTooLong,
+    Times__SelectWhenServiceAvailable,
+    //todo: we might have to have individual error ids for each time field
+    //Times__EnterWeekdaysStartTime,
+    //Times__EnterWeekdaysFinishesTime,
+    //Times__EnterWeekendsStartTime,
+    //Times__EnterWeekendsFinishesTime,
+    Times__EnterWeekdaysTimes,
+    Times__EnterWeekendsTimes,
+    Times__EnterValidWeekdaysStartTime,
+    Times__EnterValidWeekdaysFinishesTime,
+    Times__EnterValidWeekendsStartTime,
+    Times__EnterValidWeekendsFinishesTime
 }
--- src/FamilyHubs.ServiceDirectory.Admin.Web/Errors/PossibleErrors.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Errors/PossibleErrors.cs
@@ -23,5 +23,12 @@ public static class PossibleErrors
             .Add(ErrorId.What_Language__EnterSupportedLanguage, "Enter an available language")
             .Add(ErrorId.Service_Cost__MissingSelection, "Select whether it costs money to use this service")
             .Add(ErrorId.Service_Cost__DescriptionTooLong, "Cost description must be 150 characters or less")
+            .Add(ErrorId.Times__SelectWhenServiceAvailable, "Select when the service is available")
+            .Add(ErrorId.Times__EnterWeekdaysTimes, "Enter times for weekdays")
+            .Add(ErrorId.Times__EnterWeekendsTimes, "Enter times for weekends")
+            .Add(ErrorId.Times__EnterValidWeekdaysStartTime, "Enter a valid start time for weekdays")
+            .Add(ErrorId.Times__EnterValidWeekdaysFinishesTime, "Enter a valid finish time for weekdays")
+            .Add(ErrorId.Times__EnterValidWeekendsStartTime, "Enter a valid start time for weekends")
+            .Add(ErrorId.Times__EnterValidWeekendsFinishesTime, "Enter a valid finish time for weekends")
         ;
 }
