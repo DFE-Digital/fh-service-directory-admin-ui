8c37af7a2c0dc7711f839563708273428355217d
Phil Davies <github@phildavies.co.uk>
2024-01-17 13:38:48 +0000

    fix warning

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/Placeholder.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/Placeholder.cshtml.cs
@@ -1,12 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
+
+public class PlaceholderModel : PageModel
 {
-    public class PlaceholderModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
 }
