a847b14877727e4824d0d72ba17f6c54ba2c2c18
Phil Davies <github@phildavies.co.uk>
2024-01-25 12:51:02 +0000

    wip

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Core/Models/LocationModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FamilyHubs.ServiceDirectory.Admin.Core.Models;
+
+//todo: common base for Location and Service models
+
+public class LocationModel : LocationModel<object>
+{
+}
+
+public class LocationModel<T>
+{
+    public string? Description { get; set; }
+
+    public LocationErrorState? ErrorState { get; set; }
+
+    public string? UserInputType { get; set; }
+    public T? UserInput { get; set; }
+}
