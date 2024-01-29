89a037c4fbc58496a74aa96d390e5804e503c3b2
Phil Davies <github@phildavies.co.uk>
2024-01-29 12:04:36 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/Location-Details.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/Location-Details.cshtml.cs
@@ -3,6 +3,7 @@ using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 using FamilyHubs.SharedKernel.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_locations;
 
@@ -12,5 +13,10 @@ public class Location_DetailsModel : LocationPageModel
     public Location_DetailsModel(IRequestDistributedCache connectionRequestCache)
         : base(LocationJourneyPage.Location_Details, connectionRequestCache)
     {
+    }
+
+    protected override Task<IActionResult> OnPostWithModelAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
     }
 }
