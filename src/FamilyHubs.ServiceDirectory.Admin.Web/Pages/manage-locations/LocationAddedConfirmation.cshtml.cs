57e98c2dfa1599521d7e99b531b96bf08ddebe82
Witek <witek.poreda@education.gov.uk>
2024-01-30 11:27:54 +0000

    add Authorize

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/LocationAddedConfirmation.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/LocationAddedConfirmation.cshtml.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Mvc;
+using FamilyHubs.SharedKernel.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_locations
 {
+    [Authorize(Roles = RoleGroups.AdminRole)]
     public class LocationAddedConfirmationModel : PageModel
     {
         public void OnGet()
