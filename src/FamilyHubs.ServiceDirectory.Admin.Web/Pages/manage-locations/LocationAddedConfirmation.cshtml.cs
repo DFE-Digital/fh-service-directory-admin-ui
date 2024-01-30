a981b1053b399c3a5cbc2c73f9042141b4416e88
Witek <witek.poreda@education.gov.uk>
2024-01-30 12:09:11 +0000

    use HeaderPageModel

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/LocationAddedConfirmation.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/LocationAddedConfirmation.cshtml.cs
@@ -1,11 +1,11 @@
+using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 using FamilyHubs.SharedKernel.Identity;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_locations
 {
     [Authorize(Roles = RoleGroups.AdminRole)]
-    public class LocationAddedConfirmationModel : PageModel
+    public class LocationAddedConfirmationModel : HeaderPageModel
     {
         public void OnGet()
         {
