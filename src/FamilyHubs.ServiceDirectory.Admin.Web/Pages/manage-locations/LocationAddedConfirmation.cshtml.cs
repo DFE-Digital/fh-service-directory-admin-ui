b90d20d062e89a631ba1cb9c4b3052890de60ceb
Witek <witek.poreda@education.gov.uk>
2024-01-30 11:16:20 +0000

    FHG-4778 Location added page

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/LocationAddedConfirmation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_locations
+{
+    public class LocationAddedConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
