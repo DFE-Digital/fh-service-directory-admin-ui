2b18773b590f600720c960e24b5da6888b5d504b
Phil Davies <github@phildavies.co.uk>
2024-02-02 12:21:20 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/Location-Details.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-locations/Location-Details.cshtml.cs
@@ -48,9 +48,20 @@ public class Location_DetailsModel : LocationPageModel
     protected override async Task<IActionResult> OnPostWithModelAsync(CancellationToken cancellationToken)
     {
         var postcodeInfo = await GetPostcodeInfo(LocationModel!.Postcode!, cancellationToken);
-        await AddLocation(postcodeInfo, cancellationToken);
 
-        return RedirectToPage("/manage-locations/LocationAddedConfirmation");
+        switch (Flow)
+        {
+            case JourneyFlow.Add:
+                await AddLocation(postcodeInfo, cancellationToken);
+                return RedirectToPage("/manage-locations/LocationAddedConfirmation");
+
+            case JourneyFlow.Edit:
+                await UpdateLocation(postcodeInfo, cancellationToken);
+                return RedirectToPage("/manage-locations/LocationUpdatedConfirmation");
+
+            default:
+                throw new InvalidOperationException($"Unexpected flow: {Flow}");
+        }
     }
 
     private async Task<IPostcodeInfo> GetPostcodeInfo(string postcode, CancellationToken cancellationToken)
@@ -67,6 +78,10 @@ public class Location_DetailsModel : LocationPageModel
 
     private async Task AddLocation(IPostcodeInfo postcodeInfo, CancellationToken cancellationToken)
     {
+        // this would be cleaner, but location has required properties
+        //var location = new LocationDto();
+        //UpdateLocationFromCache(location, postcodeInfo);
+
         var location = new LocationDto
         {
             LocationType = LocationModel!.IsFamilyHub!.Value ? LocationType.FamilyHub : LocationType.NotSet,
@@ -84,5 +99,35 @@ public class Location_DetailsModel : LocationPageModel
         };
 
         await _serviceDirectoryClient.CreateLocation(location, cancellationToken);
+    }
+
+    private async Task UpdateLocation(IPostcodeInfo postcodeInfo, CancellationToken cancellationToken)
+    {
+        var location = await _serviceDirectoryClient.GetLocationById(LocationId!.Value, cancellationToken);
+        if (location is null)
+        {
+            //todo: better exception?
+            throw new InvalidOperationException($"Location not found: {LocationId}");
+        }
+
+        UpdateLocationFromCache(location, postcodeInfo);
+
+        await _serviceDirectoryClient.UpdateLocation(location, cancellationToken);
+    }
+
+    private void UpdateLocationFromCache(LocationDto location, IPostcodeInfo postcodeInfo)
+    {
+        location.LocationType = LocationModel!.IsFamilyHub!.Value ? LocationType.FamilyHub : LocationType.NotSet;
+        location.Description = LocationModel.Description;
+        location.Name = LocationModel.Name ?? "";
+        location.Address1 = LocationModel.AddressLine1!;
+        location.Address2 = LocationModel.AddressLine2 ?? "";
+        location.City = LocationModel.City!;
+        location.StateProvince = LocationModel.County ?? "";
+        location.PostCode = LocationModel.Postcode!;
+        location.Country = "UK";
+        //todo: better for API to add this?
+        location.Latitude = postcodeInfo.Latitude!.Value;
+        location.Longitude = postcodeInfo.Longitude!.Value;
     }
 }
