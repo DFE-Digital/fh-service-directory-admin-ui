667f821676b5fb67d70905da0408ccc47a2223e4
Phil Davies <github@phildavies.co.uk>
2024-02-09 15:11:19 +0000

    new times page wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/times.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/times.cshtml.cs
@@ -1,148 +1,60 @@
 using FamilyHubs.ServiceDirectory.Admin.Core.DistributedCache;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
-using FamilyHubs.ServiceDirectory.Admin.Web.ViewModel;
-using FamilyHubs.SharedKernel.Razor.Time;
+using FamilyHubs.SharedKernel.Razor.ErrorNext;
+using FamilyHubs.SharedKernel.Razor.FullPages.Checkboxes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
 
-public enum DayType
+public enum DayCode
 {
-    Weekdays,
-    Weekends
+    MO, TU, WE, TH, FR, SA, SU
 }
 
-public class timesModel : ServicePageModel<TimesModels>
+public class timesModel : ServicePageModel<TimesModels>, ICheckboxesPageModel
 {
-    [BindProperty]
-    public List<DayType> DayTypes { get; set; }
+    public static Checkbox[] StaticCheckboxes => new[]
+    {
+        new Checkbox("Monday", DayCode.MO.ToString()),
+        new Checkbox("Tuesday", DayCode.TU.ToString()),
+        new Checkbox("Wednesday", DayCode.WE.ToString()),
+        new Checkbox("Thursday", DayCode.TH.ToString()),
+        new Checkbox("Friday", DayCode.FR.ToString()),
+        new Checkbox("Saturday", DayCode.SA.ToString()),
+        new Checkbox("Sunday", DayCode.SU.ToString())
+    };
 
-    public TimesViewModels? TimesViewModels { get; set; }
+    public IEnumerable<ICheckbox> Checkboxes => StaticCheckboxes;
+
+    [BindProperty]
+    public IEnumerable<string> SelectedValues { get; set; } = Enumerable.Empty<string>();
+
+    //todo: do we want Errors in ICheckboxesPageModel?
+    public IErrorState Errors { get; set; } = ErrorState.Empty;
+
+    public string? DescriptionPartial => "Checkbox-Custom-Content";
+    public string? Legend => "Select all the days when this service is available";
+    public string? Hint => "Select all options that apply. If none apply or you do not know these yet, leave blank and click continue.";
+
+    public bool ShowSelection { get; set; }
 
     public timesModel(IRequestDistributedCache connectionRequestCache)
         : base(ServiceJourneyPage.Times, connectionRequestCache)
     {
-        DayTypes = new List<DayType>();
-    }
-
-    protected override void OnGetWithError()
-    {
-        //todo: could have array of components and models and zip them
-        TimesViewModels = new TimesViewModels(ServiceModel!.UserInput);
-
-        //todo: pass to TimesViewModels ctor? then could make view model immutable
-        TimesViewModels.WeekdaysStarts.Error = Errors.GetErrorIfTriggered(
-            (int)ErrorId.Times__EnterWeekdaysStartTime,
-            (int)ErrorId.Times__EnterValidWeekdaysStartTime);
-
-        TimesViewModels.WeekdaysFinishes.Error = Errors.GetErrorIfTriggered(
-            (int)ErrorId.Times__EnterWeekdaysFinishTime,
-            (int)ErrorId.Times__EnterValidWeekdaysFinishTime);
-
-        TimesViewModels.WeekendsStarts.Error = Errors.GetErrorIfTriggered(
-            (int)ErrorId.Times__EnterWeekendsStartTime,
-            (int)ErrorId.Times__EnterValidWeekendsStartTime);
-
-        TimesViewModels.WeekendsFinishes.Error = Errors.GetErrorIfTriggered(
-            (int)ErrorId.Times__EnterWeekendsFinishTime,
-            (int)ErrorId.Times__EnterValidWeekendsFinishTime);
     }
 
     protected override void OnGetWithModel()
     {
-        TimesViewModels = new TimesViewModels(ServiceModel!.Times);
+        //todo: from cache
+        SelectedValues = new[] { DayCode.MO.ToString(), DayCode.SU.ToString() };
     }
 
     protected override IActionResult OnPostWithModel()
     {
-        if (!DayTypes.Any())
-        {
-            return RedirectToSelf(ErrorId.Times__SelectWhenServiceAvailable);
-        }
-
-        bool weekdays = DayTypes.Contains(DayType.Weekdays);
-        bool weekends = DayTypes.Contains(DayType.Weekends);
-
-        var timesModels = TimesViewModels.GetTimesFromForm(weekdays, weekends, Request.Form);
-
-        var errors = GetTimeErrors(timesModels);
-        if (errors.Any())
-        {
-            return RedirectToSelf(timesModels, errors.ToArray());
-        }
-
-        ClearTimesIfDayTypeNotSelected(timesModels);
-
-        //todo: would check if updated here, but this page is changing
-
-        ServiceModel!.Times = timesModels;
+        //todo: into cache
+        //ServiceModel!.Times = SelectedValues;
 
         return NextPage();
-    }
-
-    private void ClearTimesIfDayTypeNotSelected(TimesModels timesModels)
-    {
-        // if checkbox not ticked, don't save any entered values
-        if (!timesModels.Weekdays)
-        {
-            timesModels.WeekdaysStarts = TimeModel.Empty;
-            timesModels.WeekdaysFinishes = TimeModel.Empty;
-        }
-
-        if (!timesModels.Weekends)
-        {
-            timesModels.WeekendsStarts = TimeModel.Empty;
-            timesModels.WeekendsFinishes = TimeModel.Empty;
-        }
-    }
-
-    private List<ErrorId> GetTimeErrors(TimesModels timesModels)
-    {
-        List<ErrorId> errors = new();
-
-        if (timesModels.Weekdays)
-        {
-            if (timesModels.WeekdaysStarts.IsEmpty)
-            {
-                errors.Add(ErrorId.Times__EnterWeekdaysStartTime);
-            }
-            else if (!timesModels.WeekdaysStarts.IsValid)
-            {
-                errors.Add(ErrorId.Times__EnterValidWeekdaysStartTime);
-            }
-
-            if (timesModels.WeekdaysFinishes.IsEmpty)
-            {
-                errors.Add(ErrorId.Times__EnterWeekdaysFinishTime);
-            }
-            else if (!timesModels.WeekdaysFinishes.IsValid)
-            {
-                errors.Add(ErrorId.Times__EnterValidWeekdaysFinishTime);
-            }
-        }
-
-        if (timesModels.Weekends)
-        {
-            if (timesModels.WeekendsStarts.IsEmpty)
-            {
-                errors.Add(ErrorId.Times__EnterWeekendsStartTime);
-            }
-            else if (!timesModels.WeekendsStarts.IsValid)
-            {
-                errors.Add(ErrorId.Times__EnterValidWeekendsStartTime);
-            }
-
-            if (timesModels.WeekendsFinishes.IsEmpty)
-            {
-                errors.Add(ErrorId.Times__EnterWeekendsFinishTime);
-            }
-            else if (!timesModels.WeekendsFinishes.IsValid)
-            {
-                errors.Add(ErrorId.Times__EnterValidWeekendsFinishTime);
-            }
-        }
-
-        return errors;
     }
 }
