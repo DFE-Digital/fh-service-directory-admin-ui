0574ce463429bce51752eb96aff715f2af82b4b7
Phil Davies <github@phildavies.co.uk>
2024-02-20 14:20:46 +0000

    select location wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/Select-Location.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/Select-Location.cshtml.cs
@@ -1,15 +1,69 @@
+using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
 using FamilyHubs.ServiceDirectory.Admin.Core.DistributedCache;
 using FamilyHubs.ServiceDirectory.Admin.Core.Models;
 using FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
+using FamilyHubs.ServiceDirectory.Shared.Dto;
+using FamilyHubs.ServiceDirectory.Shared.Models;
+using FamilyHubs.SharedKernel.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading;
 
 namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
 
 public class Select_LocationModel : ServicePageModel
 {
-    public Select_LocationModel(IRequestDistributedCache connectionRequestCache)
+    private readonly IServiceDirectoryClient _serviceDirectoryClient;
+
+    public Select_LocationModel(
+        IServiceDirectoryClient serviceDirectoryClient,
+        IRequestDistributedCache connectionRequestCache)
         : base(ServiceJourneyPage.Select_Location, connectionRequestCache)
     {
+        _serviceDirectoryClient = serviceDirectoryClient;
+    }
+
+    protected override async Task OnGetWithModelAsync(CancellationToken cancellationToken)
+    {
+        //todo: PRG and pass the search name through user input
+        string searchName = "";
+
+        long organisationId = long.Parse(FamilyHubsUser.OrganisationId);
+
+        //todo: should have a single PaginatedList?
+        var locations = await GetLocations(searchName, organisationId, cancellationToken);
+    }
+
+    private async Task<string> GetOrganisationName(long organisationId)
+    {
+        var organisation = await _serviceDirectoryClient.GetOrganisationById(organisationId);
+        return organisation.Name;
+    }
+
+    private async Task<List<LocationDto>> GetLocations(
+        string searchName,
+        long organisationId,
+        CancellationToken cancellationToken)
+    {
+        // we ask for a maximum of 10000, as the front end is limited to 10000 anyway, see https://chromium.googlesource.com/chromium/blink.git/+/master/Source/core/html/HTMLSelectElement.cpp#77
+        const int maxLocations = 10000;
+
+        //todo: should have a single PaginatedList?
+        PaginatedList<LocationDto> locations;
+
+        if (FamilyHubsUser.Role == RoleTypes.DfeAdmin)
+        {
+            //todo: as an optimisation, could have a version without sorting etc.
+            //todo: some of these are mandatory in the client, but not in the api - refactor params
+            // passing "" as orderbyColumn should mean no ordering is done, which is ideal for us
+
+            locations = await _serviceDirectoryClient.GetLocations(null, "", searchName, null, 1, maxLocations, cancellationToken);
+        }
+        else
+        {
+            locations = await _serviceDirectoryClient.GetLocationsByOrganisationId(organisationId, null, "", searchName, null, 1, maxLocations, cancellationToken);
+        }
+
+        return locations.Items;
     }
 
     protected override IActionResult OnPostWithModel()
