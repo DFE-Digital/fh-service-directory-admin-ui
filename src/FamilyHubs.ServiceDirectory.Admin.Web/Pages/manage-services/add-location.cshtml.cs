a63ee08490f1a418ef78f3e2ecc9dce28c4fff72
Phil Davies <github@phildavies.co.uk>
2024-03-11 15:31:34 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/add-location.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/add-location.cshtml.cs
@@ -43,6 +43,11 @@ public class Add_LocationModel : ServicePageModel, IRadiosPageModel
 
         ServiceModel!.AddingLocations = addLocations;
 
+        if (!addLocations)
+        {
+            ServiceModel!.CurrentLocation = null;
+        }
+
         return NextPage();
     }
 
