4426a275121e0ca72c8b226914e493f71aa0ce82
Phil Davies <github@phildavies.co.uk>
2024-04-24 14:06:05 +0100

    wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Journeys/LocationJourneyPageExtensions.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Journeys/LocationJourneyPageExtensions.cs
@@ -17,17 +17,17 @@ public static class LocationJourneyPageExtensions
     }
 
     //todo: flow is only needed for initiator page. move initiator logic to where called for initiator page and remove flow param?
-    public static string GetPagePath(this LocationJourneyPage page, JourneyFlow flow)
+    public static string GetPagePath(this LocationJourneyPage page, LocationJourneyFlow flow)
     {
         if (page == LocationJourneyPage.Initiator)
         {
             switch (flow)
             {
-                case JourneyFlow.Add:
-                case JourneyFlow.AddRedo:
+                case LocationJourneyFlow.Add:
+                //case JourneyFlow.AddRedo:
                     //todo: consumers are going to add query params to welcome, which aren't needed
                     return "/manage-locations";
-                case JourneyFlow.Edit:
+                case LocationJourneyFlow.Edit:
                     // details is both the initiator and the final page of the journey
                     page = LocationJourneyPage.Location_Details;
                     break;
