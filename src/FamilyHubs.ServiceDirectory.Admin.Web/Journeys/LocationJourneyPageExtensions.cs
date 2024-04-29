69a6a684b0c72a382e585b4208106e725ee1a0b2
Phil Davies <github@phildavies.co.uk>
2024-04-29 15:48:59 +0100

    tidy up

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Journeys/LocationJourneyPageExtensions.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Journeys/LocationJourneyPageExtensions.cs
@@ -17,24 +17,6 @@ public static class LocationJourneyPageExtensions
 
     public static string GetPagePath(this LocationJourneyPage page, JourneyFlow flow)
     {
-        //if (page == LocationJourneyPage.Initiator)
-        //{
-        //    switch (flow)
-        //    {
-        //        case JourneyFlow.Add:
-        //            //todo: consumers are going to add query params to welcome, which aren't needed
-        //            //todo: back is welcome/manage depending on the user
-        //            // can we move initiator handling our of here, as we don;t want to always have to pass in the user
-        //            return "/manage-locations";
-        //        case JourneyFlow.Edit:
-        //            // details is both the initiator and the final page of the journey
-        //            page = LocationJourneyPage.Location_Details;
-        //            break;
-        //        default:
-        //            throw new ArgumentOutOfRangeException(nameof(flow), flow, null);
-        //    }
-        //}
-
         return $"/manage-locations/{page.GetSlug()}?flow={flow.ToUrlString()}";
     }
 
@@ -45,7 +27,7 @@ public static class LocationJourneyPageExtensions
 
     public static string GetAddFlowStartPagePath(Journey journey, string? parentJourneyContext)
     {
-        //todo: add flow and journey in GetPathPath (obvs renamed)?
+        //todo: add journey in GetPathPath (obvs renamed)?
 
         string parentJourneyContextParam = parentJourneyContext == null ? "" : $"&parentJourneyContext={parentJourneyContext}";
 
@@ -54,7 +36,6 @@ public static class LocationJourneyPageExtensions
 
     public static string GetEditStartPagePath()
     {
-        //return $"/manage-locations/{LocationJourneyPage.Location_Details.GetPageUrl()}?flow={JourneyFlow.Edit}";
         return LocationJourneyPage.Location_Details.GetPagePath(JourneyFlow.Edit);
     }
 }
