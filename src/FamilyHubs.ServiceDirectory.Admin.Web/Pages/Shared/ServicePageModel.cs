67c3474da871cb21be32feb6d6fd77cee0eec368
Phil Davies <github@phildavies.co.uk>
2024-05-17 11:56:26 +0100

    declaritive mini-journey rules wip

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Shared/ServicePageModel.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/Shared/ServicePageModel.cs
@@ -15,6 +15,37 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.Shared;
 // Use Array.Exists, rather than Any() : makes refactoring harder, and doesn't look as object-oriented
 #pragma warning disable S6605
 
+//todo: rule for just mini-journey, or rules for all?
+// start with just for mini-journey
+
+//public record MiniJourneyRule(
+//    //ServiceJourneyChangeFlow? ChangeFlow,
+//    ServiceJourneyChangeFlow? ChangeFlow,
+//    ServiceJourneyPage? FirstPage = null,
+//    ServiceJourneyPage? LastPage = null)
+//    //todo: one predicate or first/last?
+//    //todo: either just hardcode this, or have one for back and one for next?
+//    //ServiceJourneyPage? OverridePage = ServiceJourneyPage.Service_Detail)
+//{
+//    private static ServiceJourneyPage OverridePage = ServiceJourneyPage.Service_Detail;
+
+//    public ServiceJourneyPage? CheckOverrideNextPage(
+//        ServiceJourneyChangeFlow? changeFlow,
+//        ServiceJourneyPage currentPage) // nextpage too?
+//    {
+//        return changeFlow == ChangeFlow && currentPage >= LastPage
+//            ? OverridePage : null;
+//    }
+
+//    public ServiceJourneyPage? CheckOverrideBackPage(
+//        ServiceJourneyChangeFlow? changeFlow,
+//        ServiceJourneyPage currentPage) // nextpage too?
+//    {
+//        return changeFlow == ChangeFlow && currentPage <= FirstPage
+//            ? OverridePage : null;
+//    }
+//}
+
 public class ServicePageModel : ServicePageModel<object>
 {
     protected ServicePageModel(
@@ -29,6 +60,17 @@ public class ServicePageModel : ServicePageModel<object>
 public class ServicePageModel<TInput> : HeaderPageModel
     where TInput : class?
 {
+    //todo: declarative mini-journey rules, rather than hand coding
+    //private static readonly MiniJourneyRule[] MiniJourneyRules =
+    //{
+    //    new(ServiceJourneyChangeFlow.SinglePage),
+    //    new(ServiceJourneyChangeFlow.LocalAuthority, ServiceJourneyPage.Local_Authority, ServiceJourneyPage.Vcs_Organisation),
+    //    //todo: these will need nextpage/ previouspage after core calc
+    //    // convert 2 above to use nextpage/ previouspage (or could support current and next/previous)
+    //    //new(ServiceJourneyChangeFlow.Location),
+    //    //new(ServiceJourneyChangeFlow.HowUse),
+    //};
+
     //todo: make non-nullable any that are guaranteed to be set in get/post?
     public JourneyFlow Flow { get; set; }
     public ServiceJourneyChangeFlow? ChangeFlow { get; set; }
