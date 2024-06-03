12a0ab3761580b6744eb79de4a9668f02ef289cf
Phil Davies <github@phildavies.co.uk>
2024-06-03 15:38:11 +0100

    wip

---
+++ src/FamilyHubs.ServiceDirectory.Admin.Core/ApiClient/AiClient.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.Http.Json;
+using FamilyHubs.SharedKernel.HealthCheck;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using FamilyHubs.Notification.Api.Client.Exceptions;
+
+namespace FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
+
+public interface IAiClient
+{
+    //    Task<ContentCheckResponse> Call(IDictionary<string, string> content, CancellationToken cancellationToken = default);
+    Task<ContentCheckResponse> Call(string content, CancellationToken cancellationToken = default);
+}
+
+public record Instance(string Reason, string Content);
+
+public record Category(bool Flag, List<Instance> Instances);
+
+public record ContentCheckResponse(
+    int ReadingLevel,
+    Category InappropriateLanguage,
+    Category Security,
+    Category PoliticisedSentiment,
+    Category PII,
+    Category GrammarAndSpelling,
+    Category StyleViolations
+);
+
+public class AiClient : IAiClient //, IHealthCheckUrlGroup
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private static string? _endpoint;
+    internal const string HttpClientName = "ai";
+
+    public AiClient(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    //todo: accept a collection of content for a service/location
+    // use enum for ids
+    public async Task<ContentCheckResponse> Call(string content, CancellationToken cancellationToken = default)//IDictionary<string, string> content, CancellationToken cancellationToken = default)
+    {
+        var httpClient = _httpClientFactory.CreateClient(HttpClientName);
+
+        using var response = await httpClient.PostAsJsonAsync("/v1/completions", content, cancellationToken);
+
+        if (!response.IsSuccessStatusCode)
+            //throw new AiClientException(response, await response.Content.ReadAsStringAsync(cancellationToken));
+            throw new InvalidOperationException("Error calling AI endpoint");
+
+        var contentCheckResponse = await JsonSerializer.DeserializeAsync<ContentCheckResponse>(
+            await response.Content.ReadAsStreamAsync(cancellationToken),
+            cancellationToken: cancellationToken);
+
+        if (contentCheckResponse is null)
+        {
+            // the only time it'll be null, is if the API returns "null"
+            // (see https://stackoverflow.com/questions/71162382/why-are-the-return-types-of-nets-system-text-json-jsonserializer-deserialize-m)
+            // unlikely, but possibly (pass new MemoryStream(Encoding.UTF8.GetBytes("null")) to see it actually return null)
+            // note we hard-code passing "null", rather than messing about trying to rewind the stream, as this is such a corner case and we want to let the deserializer take advantage of the async stream (in the happy case)
+            //throw new AiClientException(response, "null");
+            throw new InvalidOperationException("Error calling AI endpoint");
+        }
+
+        return contentCheckResponse;
+    }
+
+    internal static string GetEndpoint(IConfiguration configuration)
+    {
+        const string endpointConfigKey = "FamilyHubsUi:Urls:Ai";
+
+        // as long as the config isn't changed, the worst that can happen is we fetch more than once
+        return _endpoint ??= ConfigurationException.ThrowIfNotUrl(
+            endpointConfigKey,
+            configuration[endpointConfigKey],
+            "The AI URL", "http://localhost:1234");
+    }
+
+    //public static Uri HealthUrl(IConfiguration configuration)
+    //{
+    //    return new Uri(new Uri(GetEndpoint(configuration)), "SW1A2AA");
+    //}
+}
