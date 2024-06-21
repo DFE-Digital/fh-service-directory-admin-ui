61d989eeb8e9d8d78a28a4cc22f8a467bcf85f01
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-06-21 10:20:01 +0100

    Refactor healthcheck

--- src/FamilyHubs.ServiceDirectory.Admin.Web/StartupExtensions.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/StartupExtensions.cs
@@ -5,6 +5,7 @@ using FamilyHubs.ServiceDirectory.Admin.Core.Health;
 using FamilyHubs.ServiceDirectory.Admin.Core.Services;
 using FamilyHubs.SharedKernel.GovLogin.AppStart;
 using FamilyHubs.SharedKernel.Identity;
+using FamilyHubs.SharedKernel.Razor.Health;
 using FamilyHubs.SharedKernel.Services.PostcodesIo.Extensions;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Mvc;
@@ -249,6 +250,6 @@ public static class StartupExtensions
 
         app.MapRazorPages();
 
-        app.MapFamilyHubsHealthChecks();
+        app.MapFamilyHubsHealthChecks(typeof(StartupExtensions).Assembly);
     }
 }
