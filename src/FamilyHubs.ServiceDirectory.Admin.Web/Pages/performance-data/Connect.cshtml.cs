43b8f43e91c6e52b66f6e62de200098ca63681e4
Aaron Yarborough <me@aaronjy.me>
2024-06-28 14:39:04 +0100

    fix: swap servicetypeid to show correct reports

--- src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/performance-data/Connect.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/performance-data/Connect.cshtml.cs
@@ -46,8 +46,8 @@ public class ConnectPerformanceDataModel : HeaderPageModel
 
         OrgName = organisation?.Name;
 
-        var searches = await _reportingClient.GetServicesSearchesTotal(ServiceType.FamilyExperience, organisationId, cancellationToken);
-        var searchesPast7Days = await _reportingClient.GetServicesSearchesPast7Days(ServiceType.FamilyExperience, organisationId, cancellationToken);
+        var searches = await _reportingClient.GetServicesSearchesTotal(ServiceType.InformationSharing, organisationId, cancellationToken);
+        var searchesPast7Days = await _reportingClient.GetServicesSearchesPast7Days(ServiceType.InformationSharing, organisationId, cancellationToken);
 
         Totals = new Dictionary<PerformanceDataType, long>
         {
@@ -59,6 +59,6 @@ public class ConnectPerformanceDataModel : HeaderPageModel
             { PerformanceDataType.SearchesTotal, searchesPast7Days }
         };
 
-        Breakdown = await _reportingClient.GetServicesSearches4WeekBreakdown(ServiceType.FamilyExperience, organisationId, cancellationToken);
+        Breakdown = await _reportingClient.GetServicesSearches4WeekBreakdown(ServiceType.InformationSharing, organisationId, cancellationToken);
     }
 }
